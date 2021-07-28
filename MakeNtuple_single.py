aa386d47ea45861e3021f413694776c39a3c4d83
PARK Junewoo <junewoo@ccw01.cc.kek.jp>
2021-07-28 12:06:23 +0900

    MakeNtuple_single.py - add good pi0

--- MakeNtuple_single.py
+++ MakeNtuple_single.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # usage: basf2 MakeNtuple_multi.py "./20210402/evt-0.mdst"
-# last: 2021-07-19-1
+# last: 2021-07-28-0
 
 import os
 import sys
@@ -71,7 +71,8 @@ if monitoring:
 ma.fillParticleList(decayString="K+:mychargedKaon", cut="atcPIDBelle(3,2)>0.6 and eIDBelle < 0.9 and muIDBelle < 0.9 and dr < 2 and dz < 4",path=my_path)
 ma.fillParticleList(decayString="K+:all", cut="", path=my_path)
 ma.cutAndCopyList('K_S0:good', 'K_S0:mdst', cut='goodBelleKshort', path=my_path)
-ma.cutAndCopyList("pi0:good", "pi0:mdst", cut="M>0.118 and M<0.15", path=my_path)
+ma.cutAndCopyList("pi0:good", "pi0:mdst", cut="M>0.118 and M<0.15 and daughter(0,clusterE9E21)>0.95 and daughter(1,clusterE9E21)>0.95 and abs(formula((daughter(0,E)-daughter(1,E))/(daughter(0,E)+daughter(1,E))))<0.9", path=my_path)
+
 #ma.cutAndCopyList("gamma:good", "gamma:mdst", cut="E>0.05",path=my_path)
 #ma.fillParticleList(decayString="pi+:all", cut="", path=my_path)
 #ma.fillParticleList(decayString="gamma:all", cut="", path=my_path)
