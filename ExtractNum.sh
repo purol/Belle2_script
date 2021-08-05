8c7549c6ccad4f0ab5fd1f3fc08be991bd23aa3f
PARK Junewoo <junewoo@ccw03.cc.kek.jp>
2021-08-05 10:14:56 +0900

    ExtractNum.sh - argument is now directory

--- ExtractNum.sh
+++ ExtractNum.sh
@@ -1,12 +1,15 @@
 N_B2BIIFixMdst=0
 N_B2BIIConvertMdst=0
-temp=0
-for file in "$@"
+N_RootOutput=0
+for file in $(find "$1" -name "*.log")
 do
     temp=`cat $file | grep "B2BIIFixMdst   " | cut -d"|" -f2 | tr -d " "`
     temp2=`cat $file | grep "B2BIIConvertMdst   " | cut -d"|" -f2 | tr -d " "`
+    temp3=`cat $file | grep "RootOutput   " | cut -d"|" -f2 | tr -d " "`
     N_B2BIIFixMdst=`expr $N_B2BIIFixMdst + $temp`
     N_B2BIIConvertMdst=`expr $N_B2BIIConvertMdst + $temp2`
+    N_RootOutput=`expr $N_RootOutput + $temp3`
 done
 echo "the number of total event: $N_B2BIIFixMdst"
 echo "the number of total event after FixMdst: $N_B2BIIConvertMdst"
+echo "the number of total event after Skimming: $N_RootOutput"
