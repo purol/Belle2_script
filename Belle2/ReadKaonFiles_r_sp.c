a2f31bcc9db61ee4a7832da4eb079ddbdca60966
purol <purol0812@gmail.com>
2021-09-07 11:22:41 +0900

    read kaon root file

---
+++ Belle2/ReadKaonFiles_r_sp.c
@@ -0,0 +1,264 @@
+void load_files(const char *dirname, std::vector<string>* names){
+   TSystemDirectory dir(dirname, dirname);
+   TList *files = dir.GetListOfFiles();
+   if (files) {
+      TSystemFile *file;
+      TString fname;
+      TIter next(files);
+      while ((file=(TSystemFile*)next())) {
+         fname = file->GetName();
+         if (!file->IsDirectory() && fname.EndsWith(".root")) {
+            names->push_back(fname.Data());
+         }
+      }
+   }
+}
+
+typedef struct data{
+    int __experiment__;
+    int __run__;
+    int __event__;
+    int __candidate__;
+    int __ncandidates__;
+
+    double variables[6];
+    // 0: kaonID, 1: nCDCHits, 2: dr
+    // 3: dz, 4: M, 5: dM
+
+} Data; 
+
+class Loader {
+public:
+    enum Variable
+    {
+        variables = 0
+    };
+    enum Inequality
+    {
+        larger_than = 0,
+        smaller_than
+    };
+    enum ValueOption {
+        Linear = 0,
+        Log
+    };
+
+private:
+    std::queue<Data> TotalData;
+
+    std::vector<int> N_events;
+    std::vector<int> N_candidates;
+    std::vector<std::string> titles;
+    int current_N_event;
+    int current_N_candidate;
+
+public:
+    Loader();
+    void initialize();
+    void GetData(TFile* input_file);
+    bool event_info_is_valid();
+    void DrawTH1F(const char* name, const char* title, int nbins, double x_low, double x_high, Loader::Variable variable, int i, Loader::ValueOption dr = Loader::Linear);
+    void PrintInformation(std::string title);
+    void Cut(Loader::Variable variable, int i, Loader::Inequality inq, double value);
+    void End();
+};
+
+Loader::Loader() {
+    std::queue<Data> empty;
+    std::swap(TotalData, empty);
+    current_N_event = 0;
+    current_N_candidate = 0;
+}
+
+void Loader::initialize() {
+    std::queue<Data> empty;
+    std::swap(TotalData, empty);
+    current_N_event = 0;
+    current_N_candidate = 0;
+}
+
+void Loader::GetData(TFile* input_file) {
+    TTree* tree_Kaon = (TTree*)input_file->Get("Kaon");
+
+    Data temp = { 0 };
+
+    // get event_info
+    tree_Kaon->SetBranchAddress("__experiment__", &temp.__experiment__);
+    tree_Kaon->SetBranchAddress("__run__", &temp.__run__);
+    tree_Kaon->SetBranchAddress("__event__", &temp.__event__);
+    tree_Kaon->SetBranchAddress("__candidate__", &temp.__candidate__);
+    tree_Kaon->SetBranchAddress("__ncandidates__", &temp.__ncandidates__);
+
+    // get Upsilon_info
+    tree_Kaon->SetBranchAddress("kaonID", &temp.variables[0]);
+    tree_Kaon->SetBranchAddress("nCDCHits", &temp.variables[1]);
+    tree_Kaon->SetBranchAddress("dr", &temp.variables[2]);
+    tree_Kaon->SetBranchAddress("dz", &temp.variables[3]);
+    tree_Kaon->SetBranchAddress("M", &temp.variables[4]);
+    tree_Kaon->SetBranchAddress("dM", &temp.variables[5]);
+
+    printf("%lld entries...\n", tree_Kaon->GetEntries());
+    for (unsigned int j = 0; j < tree_Kaon->GetEntries(); j++) { // Fill
+        tree_Kaon->GetEntry(j);
+        TotalData.push(temp);
+    }
+    input_file->Close();
+}
+
+bool Loader::event_info_is_valid() {
+    std::queue<Data> temp_queue;
+    while (!TotalData.empty()) {
+        Data temp_data = TotalData.front();
+        TotalData.pop();
+        temp_queue.push(temp_data);
+    }
+    TotalData = temp_queue;
+    return true;
+}
+
+void Loader::DrawTH1F(const char* name, const char* title, int nbins, double x_low, double x_high, Loader::Variable variable, int i, Loader::ValueOption dr = Loader::Linear) {
+    if (TH1Fs.size() == current_TH1F) { // allocate new hist
+        TH1F* hist = new TH1F(name, title, nbins, x_low, x_high);
+        TH1Fs.push_back(hist);
+    }
+    else if (TH1Fs.size() > current_TH1F) { // use what I have
+    }
+    else { // error
+        printf("ERROR!\n");
+        exit(1);
+    }
+
+    TH1F* temp_hist = TH1Fs.at(current_TH1F);
+    std::queue<Data> temp_queue = TotalData;
+    while (!temp_queue.empty()) {
+        Data temp_data = temp_queue.front();
+        temp_queue.pop();
+
+        if (variable == Loader::variables) {
+            if (dr == Loader::Linear) temp_hist->Fill(temp_data.variables[i]);
+            else if (dr == Loader::Log) temp_hist->Fill(TMath::Log10(temp_data.variables[i]));
+            else { printf("ERROR!\n"); exit(1); }
+        }
+        else {
+            printf("ERROR!\n");
+            exit(1);
+        }
+
+    }
+
+    current_TH1F++;
+}
+
+void Loader::PrintInformation(std::string title) {
+    typedef struct labels {
+        int __experiment__;
+        int __run__;
+        int __event__;
+        int __ncandidates__;
+    } Labels;
+    std::vector<Labels> label_list;
+
+    if (N_events.size() == current_N_event && N_candidates.size() == current_N_candidate && N_events.size() == N_candidates.size()) { // allocate new int
+        N_events.push_back(0);
+        N_candidates.push_back(0);
+        titles.push_back(title);
+    }
+    else if (N_events.size() > current_N_event && N_candidates.size() > current_N_candidate && N_events.size() == N_candidates.size() && current_N_event == current_N_candidate) { // use what I have
+    }
+    else { // error
+        printf("ERROR!\n");
+        exit(1);
+    }
+
+    std::queue<Data> temp_queue = TotalData;
+    while (!temp_queue.empty()) {
+        Data temp = temp_queue.front();
+        temp_queue.pop();
+
+        bool overlap = false;
+        for (unsigned int i = 0; i < label_list.size(); i++) {
+            if (label_list.at(i).__experiment__ == temp.__experiment__ && label_list.at(i).__run__ == temp.__run__ && label_list.at(i).__event__ == temp.__event__ && label_list.at(i).__ncandidates__ == temp.__ncandidates__) { overlap = true; }
+        }
+        if (overlap == false) {
+            N_events.at(current_N_event) = N_events.at(current_N_event) + 1;
+            Labels temp_Labels;
+            temp_Labels.__experiment__ = temp.__experiment__;
+            temp_Labels.__run__ = temp.__run__;
+            temp_Labels.__event__ = temp.__event__;
+            temp_Labels.__ncandidates__ = temp.__ncandidates__;
+            label_list.push_back(temp_Labels);
+        }
+    }
+    N_candidates.at(current_N_candidate) = N_candidates.at(current_N_candidate) + TotalData.size();
+
+    current_N_event++;
+    current_N_candidate++;
+}
+
+void Loader::Cut(Loader::Variable variable, int i, Loader::Inequality inq, double value) {
+    std::queue<Data> temp_queue;
+    while (!TotalData.empty()) {
+        Data temp_data = TotalData.front();
+        TotalData.pop();
+        if (variable == Loader::variables) {
+            if (inq == Loader::larger_than && temp_data.variables[i] > value) temp_queue.push(temp_data);
+            else if (inq == Loader::smaller_than && temp_data.variables[i] < value) temp_queue.push(temp_data);
+        }
+        else {
+            printf("ERROR!\n");
+            exit(1);
+        }
+    }
+    TotalData = temp_queue;
+}
+
+void Loader::End() {
+
+    for (int i = 0; i < N_events.size();i++) {
+        printf("%s\n", titles.at(i).c_str());
+        printf("Number of event: %d\n", N_events.at(i));
+        printf("Number of candidate: %d\n", N_candidates.at(i));
+    }
+
+    for (int i = 0; i < TH1Fs.size(); i++) {
+        TCanvas* c_temp = new TCanvas("c", "", 1500, 1200); c_temp->cd();
+        TH1Fs.at(i)->Draw("Hist"); c_temp->SaveAs((std::string(TH1Fs.at(i)->GetName()) + ".png").c_str());
+        delete c_temp;
+    }
+
+}
+
+void ReadDecayFiles_r_sp(){
+
+    std::vector<string> names;
+    const char* dirname = "/home/jwpark/storage/B02Xsnunu_decay_modes/output";
+
+    load_files(dirname, &names);
+
+    Loader loader;
+
+    for(unsigned int i = 0; i<names.size(); i++){
+        loader.initialize();
+
+        TFile *input_file = new TFile( (dirname+std::string("/")+names.at(i)).c_str(),"read");
+        printf("%s (%d/%zu)\n",("Read "+names.at(i) + "... ").c_str(), i, names.size());
+        loader.GetData(input_file);
+        if (loader.event_info_is_valid() == false) { printf("error!\n"); return; }
+
+        loader.PrintInformation(std::string("========== inital =========="));
+
+        loader.DrawTH1F("kaonID", "kaonID of kaon candidates;kaonID;evt", 100, 0, 1, Loader::variables, 0);
+        loader.DrawTH1F("nCDCHits", "nCDCHits of kaon candidates;nCDCHits;evt", 100, 0, 60, Loader::variables, 1);
+        loader.DrawTH1F("dr", "dr of kaon candidates;dr [cm];evt", 100, 0, 1.5, Loader::variables, 2);
+        loader.DrawTH1F("dz", "dz of kaon candidates;dz [cm];evt", 100, -4, 4, Loader::variables, 3);
+        loader.Cut(Loader::variables, 0, Loader::larger_than, 0.6);
+        loader.Cut(Loader::variables, 1, Loader::larger_than, 20);
+        loader.Cut(Loader::variables, 2, Loader::smaller_than, 0.5);
+        loader.Cut(Loader::variables, 3, Loader::smaller_than, 2.0);
+        loader.Cut(Loader::variables, 3, Loader::larger_than, -2.0);
+
+        loader.PrintInformation(std::string("========== kaonID > 0.6 & nCDCHits > 20 & dr < 0.5 & abs(dz) < 2.0 =========="));
+        loader.DrawTH1F("M_after_cuts", "M of kaon candidates;M [GeV];evt", 100, 0.492, 0.493, Loader::variables, 4);
+    }
+    loader.End();
+}
