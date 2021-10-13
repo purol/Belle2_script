d032aaedb3b0d1fd4acf64a714fc7f515be0889e
purol <purol0812@gmail.com>
2021-10-13 18:31:51 +0900

    add fermi motion in B2Xsnunu model

---
+++ mymodel/EvtBtoXsnunu_FERMI.h
@@ -0,0 +1,45 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
+#pragma once
+
+#include "EvtGenBase/EvtDecayIncoherent.hh"
+#include "EvtGenBase/EvtParticle.hh"
+
+namespace Belle2 {
+  class EvtBtoXsnunu_FERMI : public  EvtDecayIncoherent {
+
+  public:
+
+    EvtBtoXsnunu_FERMI() {}
+    virtual ~EvtBtoXsnunu_FERMI();
+
+    std::string getName();
+
+    EvtDecayBase* clone();
+
+    void initProbMax();
+
+    void init();
+
+    void decay(EvtParticle *p);
+
+    double dGdsbProb();
+    double FermiMomentum(double pf);
+    double FermiMomentumProb(double pb, double pf);
+
+  private:
+      double         _dGdsbProbMax;
+      double         _mb;          // b-quark mass for fermi motion
+      double         _ms;          // s-quark mass for fermi motion
+      double         _mq;          // spectator quark mass for fermi motion
+      double         _pf;          // Fermi motion parameter for fermi motion
+      double         _mxmin;
+
+  };
+
+} // Belle 2 Namespace
