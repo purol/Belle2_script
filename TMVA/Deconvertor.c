962a76efb3c40e9a6a53d6c7b072a8b12fb168f6
purol <purol0812@gmail.com>
2021-11-18 21:19:56 +0900

    deconvertor included MVA output now

--- TMVA/Deconvertor.c
+++ TMVA/Deconvertor.c
@@ -1,4 +1,4 @@
-// last update: 2021-11-10-00
+// last update: 2021-11-18-00
 // for Belle2 data
 
 /*
@@ -326,6 +326,10 @@ void Deconvertor(){
             temp_tree_Xs->Branch("nParticlesInList__boXsd__clMCch29__bc", &temp_DecayDataToTree[36]);
             temp_tree_Xs->Branch("nParticlesInList__boXsd__clMCch30__bc", &temp_DecayDataToTree[37]);
         }
+
+        temp_tree_upsilon->Branch("TMVA_BB", &temp_BB_output);
+        temp_tree_upsilon->Branch("TMVA_Continuum", &temp_Continuum_output);
+
         /*================================================================*/
 
         for (unsigned int j = 0; j < temp_tree->GetEntries(); j++) { // Fill
