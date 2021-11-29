1fcc0114db90367c2a2974541c3105693e3d3f28
purol <purol0812@gmail.com>
2021-11-29 21:16:30 +0900

    rev

---
+++ TMVA/temp_Separate.sh
@@ -0,0 +1,149 @@
+mkdir -p ./CHG_analysis/test_v001/ver0_signal/final_output/Mxs_large
+cd ./CHG_analysis/test_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root ./ReadRootFiles_r_sp.c 
+cd -
+
+mkdir -p ./CHG_analysis/test_v001/ver0_signal/final_output/Mxs_small
+cd ./CHG_analysis/test_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./CHG_analysis/train_v001/ver0_signal/final_output/Mxs_large
+cd ./CHG_analysis/train_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./CHG_analysis/train_v001/ver0_signal/final_output/Mxs_small
+cd ./CHG_analysis/train_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root ./ReadRootFiles_r_sp.c
+cd -
+
+
+mkdir -p ./MIX_analysis/test_v001/ver0_signal/final_output/Mxs_large
+cd ./MIX_analysis/test_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./MIX_analysis/test_v001/ver0_signal/final_output/Mxs_small
+cd ./MIX_analysis/test_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./MIX_analysis/train_v001/ver0_signal/final_output/Mxs_large
+cd ./MIX_analysis/train_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./MIX_analysis/train_v001/ver0_signal/final_output/Mxs_small
+cd ./MIX_analysis/train_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root ./ReadRootFiles_r_sp.c
+cd -
+
+
+mkdir -p ./UUBAR_analysis/test_v001/ver0_signal/final_output/Mxs_large
+cd ./UUBAR_analysis/test_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./UUBAR_analysis/test_v001/ver0_signal/final_output/Mxs_small
+cd ./UUBAR_analysis/test_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./UUBAR_analysis/train_v001/ver0_signal/final_output/Mxs_large
+cd ./UUBAR_analysis/train_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./UUBAR_analysis/train_v001/ver0_signal/final_output/Mxs_small
+cd ./UUBAR_analysis/train_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+
+mkdir -p ./DDBAR_analysis/test_v001/ver0_signal/final_output/Mxs_large
+cd ./DDBAR_analysis/test_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./DDBAR_analysis/test_v001/ver0_signal/final_output/Mxs_small
+cd ./DDBAR_analysis/test_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./DDBAR_analysis/train_v001/ver0_signal/final_output/Mxs_large
+cd ./DDBAR_analysis/train_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./DDBAR_analysis/train_v001/ver0_signal/final_output/Mxs_small
+cd ./DDBAR_analysis/train_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+
+mkdir -p ./SSBAR_analysis/test_v001/ver0_signal/final_output/Mxs_large
+cd ./SSBAR_analysis/test_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./SSBAR_analysis/test_v001/ver0_signal/final_output/Mxs_small
+cd ./SSBAR_analysis/test_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./SSBAR_analysis/train_v001/ver0_signal/final_output/Mxs_large
+cd ./SSBAR_analysis/train_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./SSBAR_analysis/train_v001/ver0_signal/final_output/Mxs_small
+cd ./SSBAR_analysis/train_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+
+mkdir -p ./CHARM_analysis/test_v001/ver0_signal/final_output/Mxs_large
+cd ./CHARM_analysis/test_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./CHARM_analysis/test_v001/ver0_signal/final_output/Mxs_small
+cd ./CHARM_analysis/test_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./CHARM_analysis/train_v001/ver0_signal/final_output/Mxs_large
+cd ./CHARM_analysis/train_v001/ver0_signal/final_output/Mxs_large
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_large/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
+mkdir -p ./CHARM_analysis/train_v001/ver0_signal/final_output/Mxs_small
+cd ./CHARM_analysis/train_v001/ver0_signal/final_output/Mxs_small
+scp jwpark@storage.yhep:/mnt/storage/jwpark/BKG_gbasf2/Aqua/CHG_analysis/test_v000/final_output/Mxs_small/ReadRootFiles_r_sp.c ./
+.q | root .x ./ReadRootFiles_r_sp.c
+cd -
+
