8309aa69020aeef93485e50a23d68e9eb046d5fe
purol <purol0812@gmail.com>
2022-01-09 16:04:44 +0900

    change

--- TMVA/Run_TMVA_Application.sh
+++ TMVA/Run_TMVA_Application.sh
@@ -14,3 +14,17 @@ do
     .q | root root ./'TMVAClassificationApplication.C("'$file'","'$OPTION'")'
 done
 
+find ./For_TMVA_SIGNAL -name "*larger*.root" -print0 | while read -d $'\0' file
+do
+    OPTION="large"
+    .q | root root ./'TMVAClassificationApplication.C("'$file'","'$OPTION'")'
+done
+
+find ./For_TMVA_SIGNAL -name "*smaller*.root" -print0 | while read -d $'\0' file
+do
+    OPTION="small"
+    .q | root root ./'TMVAClassificationApplication.C("'$file'","'$OPTION'")'
+done
+
+mkdir ./TMVA_output
+mv ./*.root ./TMVA_output
