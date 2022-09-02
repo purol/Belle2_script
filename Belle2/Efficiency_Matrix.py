44b260de52cd46e418f0297d109312e387ee59f0
purol <purol0812@gmail.com>
2022-09-02 16:10:46 +0900

    add histogram template drawer

--- Belle2/Efficiency_Matrix.py
+++ Belle2/Efficiency_Matrix.py
@@ -44,71 +44,71 @@ reco_list = [r"$B^{+}\rightarrow K^{+}\nu\bar{\nu}$",
              r"$B^{0}\rightarrow K^{+}K^{-}K^{+}\pi^{-}\nu\bar{\nu}$",
              r"$B^{0}\rightarrow K^{+}K^{-}K^{0}_{S}\pi^{0}\nu\bar{\nu}$"
              ]
-gen_list = [r"$B^{+}\rightarrow K^{+}J/\psi$",
-            r"$B^{+}\rightarrow K^{+}\pi^{0}J/\psi$",
-            r"$B^{+}\rightarrow K^{0}\pi^{+}J/\psi$",      
-            r"$B^{+}\rightarrow K^{+}\pi^{-}\pi^{+}J/\psi$",
-            r"$B^{+}\rightarrow K^{0}\pi^{+}\pi^{0}J/\psi$",
-            r"$B^{+}\rightarrow K^{+}\pi^{-}\pi^{+}\pi^{0}J/\psi$",
-            r"$B^{+}\rightarrow K^{0}\pi^{+}\pi^{-}\pi^{+}J/\psi$",   
-            r"$B^{+}\rightarrow K^{+}\pi^{-}\pi^{+}\pi^{-}\pi^{+}J/\psi$",
-            r"$B^{+}\rightarrow K^{0}\pi^{+}\pi^{-}\pi^{+}\pi^{0}J/\psi$",
-            r"$B^{+}\rightarrow K^{+}\pi^{0}\pi^{0}J/\psi$",
-            r"$B^{+}\rightarrow K^{0}\pi^{+}\pi^{0}\pi^{0}J/\psi$",
-            r"$B^{+}\rightarrow K^{+}\pi^{-}\pi^{+}\pi^{0}\pi^{0}J/\psi$",
-            r"$B^{+}\rightarrow K^{+}K^{-}K^{+}J/\psi$",
-            r"$B^{+}\rightarrow K^{+}K^{-}K^{0}\pi^{+}J/\psi$",
-            r"$B^{+}\rightarrow K^{+}K^{-}K^{+}\pi^{0}J/\psi$",
-            r"$B^{0}\rightarrow K^{0}J/\psi$",
-            r"$B^{0}\rightarrow K^{+}\pi^{-}J/\psi$",
-            r"$B^{0}\rightarrow K^{0}\pi^{0}J/\psi$",
-            r"$B^{0}\rightarrow K^{+}\pi^{-}\pi^{0}J/\psi$",
-            r"$B^{0}\rightarrow K^{0}\pi^{+}\pi^{-}J/\psi$",
-            r"$B^{0}\rightarrow K^{+}\pi^{-}\pi^{+}\pi^{-}J/\psi$",
-            r"$B^{0}\rightarrow K^{0}\pi^{+}\pi^{-}\pi^{0}J/\psi$",
-            r"$B^{0}\rightarrow K^{+}\pi^{-}\pi^{+}\pi^{-}\pi^{0}J/\psi$",
-            r"$B^{0}\rightarrow K^{0}\pi^{+}\pi^{-}\pi^{+}\pi^{-}J/\psi$",
-            r"$B^{0}\rightarrow K^{0}\pi^{0}\pi^{0}J/\psi$",
-            r"$B^{0}\rightarrow K^{+}\pi^{-}\pi^{0}\pi^{0}J/\psi$",
-            r"$B^{0}\rightarrow K^{0}\pi^{+}\pi^{-}\pi^{0}\pi^{0}J/\psi$",
-            r"$B^{0}\rightarrow K^{+}K^{-}K^{0}J/\psi$",
-            r"$B^{0}\rightarrow K^{+}K^{-}K^{+}\pi^{-}J/\psi$",
-            r"$B^{0}\rightarrow K^{+}K^{-}K^{0}\pi^{0}J/\psi$",
+gen_list = [r"$B^{+}\rightarrow K^{+}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{+}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{0}\pi^{+}\nu\bar{\nu}$",      
+            r"$B^{+}\rightarrow K^{+}\pi^{-}\pi^{+}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{0}\pi^{+}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{+}\pi^{-}\pi^{+}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{0}\pi^{+}\pi^{-}\pi^{+}\nu\bar{\nu}$",   
+            r"$B^{+}\rightarrow K^{+}\pi^{-}\pi^{+}\pi^{-}\pi^{+}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{0}\pi^{+}\pi^{-}\pi^{+}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{+}\pi^{0}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{0}\pi^{+}\pi^{0}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{+}\pi^{-}\pi^{+}\pi^{0}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{+}K^{-}K^{+}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{+}K^{-}K^{0}\pi^{+}\nu\bar{\nu}$",
+            r"$B^{+}\rightarrow K^{+}K^{-}K^{+}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{0}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{+}\pi^{-}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{0}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{+}\pi^{-}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{0}\pi^{+}\pi^{-}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{+}\pi^{-}\pi^{+}\pi^{-}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{0}\pi^{+}\pi^{-}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{+}\pi^{-}\pi^{+}\pi^{-}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{0}\pi^{+}\pi^{-}\pi^{+}\pi^{-}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{0}\pi^{0}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{+}\pi^{-}\pi^{0}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{0}\pi^{+}\pi^{-}\pi^{0}\pi^{0}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{+}K^{-}K^{0}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{+}K^{-}K^{+}\pi^{-}\nu\bar{\nu}$",
+            r"$B^{0}\rightarrow K^{+}K^{-}K^{0}\pi^{0}\nu\bar{\nu}$",
             r"$others$"
             ]
 
 correction = []
-correction.append(1353401.0)
-correction.append(880864.0)
-correction.append(1753251.0)
-correction.append(1973940.0)
-correction.append(2214036.0)
-correction.append(1223776.0)
-correction.append(461945.0)
-correction.append(286674.0)
-correction.append(378742.0)
-correction.append(452343.0)
-correction.append(321948.0)
-correction.append(341842.0)
-correction.append(77819.0)
-correction.append(42265.0)
-correction.append(21917.0)
-correction.append(1.0)
-correction.append(1659619.0)
-correction.append(295577.0)
-correction.append(1971422.0)
-correction.append(2604225.0)
-correction.append(960194.0)
-correction.append(1155456.0)
-correction.append(363364.0)
-correction.append(170813.0)
-correction.append(251427.0)
-correction.append(306204.0)
-correction.append(270138.0)
-correction.append(61690.0)
-correction.append(28477.0)
-correction.append(21410.0)
-correction.append(7591203.0)
+correction.append(1350.0)
+correction.append(1651.6)
+correction.append(3300.4)
+correction.append(673.9)
+correction.append(825.3)
+correction.append(473.3)
+correction.append(119.1)
+correction.append(24.6)
+correction.append(148.2)
+correction.append(127.6)
+correction.append(102.9)
+correction.append(91.8)
+correction.append(54.8)
+correction.append(21.7)
+correction.append(11.0)
+correction.append(1184.0)
+correction.append(3034.7)
+correction.append(1515.9)
+correction.append(835.5)
+correction.append(677.4)
+correction.append(120.5)
+correction.append(475.9)
+correction.append(149.8)
+correction.append(25.0)
+correction.append(129.2)
+correction.append(104.4)
+correction.append(92.4)
+correction.append(54.9)
+correction.append(14.5)
+correction.append(10.9)
+correction.append(1738.5)
 
 for i in range(0,30):
     for j in range(0,31):
@@ -121,4 +121,6 @@ plt.figure(figsize = (36,24))
 ax = sn.heatmap(df_cm, annot=True, cmap="YlGnBu", fmt='.1e')
 ax.set_xticklabels(ax.get_xticklabels(), rotation=45, fontsize = 18, horizontalalignment='right')
 ax.set_yticklabels(ax.get_yticklabels(), fontsize = 18)
+plt.xlabel(r"MC decay modes", fontsize=25)
+plt.ylabel(r"Reconstructed decay modes", fontsize=25)
 plt.savefig("eff_matrix.png", bbox_inches='tight')
---
+++ HistFactory/template_draw.C
@@ -0,0 +1,204 @@
+#include <iostream>
+#include <stdio.h>
+
+#include "TRandom3.h"
+#include "TCanvas.h"
+#include "TDatime.h"
+#include "TStopwatch.h"
+#include "TLegend.h"
+#include "TIterator.h"
+#include "TH3.h"
+#include "TLatex.h"
+
+#include "RooChi2Var.h"
+#include "RooAbsData.h"
+#include "RooRealSumPdf.h"
+#include "RooPoisson.h"
+#include "RooGaussian.h"
+#include "RooRealVar.h"
+#include "RooMCStudy.h"
+#include "RooMinuit.h"
+#include "RooCategory.h"
+#include "RooHistPdf.h"
+#include "RooSimultaneous.h"
+#include "RooExtendPdf.h"
+#include "RooDataSet.h"
+#include "RooDataHist.h"
+#include "RooFitResult.h"
+#include "RooMsgService.h"
+#include "RooParamHistFunc.h"
+#include "RooHist.h"
+#include "RooRandom.h"
+
+#include "RooStats/ModelConfig.h"
+#include "RooStats/ToyMCSampler.h"
+#include "RooStats/MinNLLTestStat.h"
+
+#include "RooStats/HistFactory/FlexibleInterpVar.h"
+#include "RooStats/HistFactory/PiecewiseInterpolation.h"
+#include "RooStats/HistFactory/HistFactorySimultaneous.h"
+#include "RooStats/HistFactory/Channel.h"
+#include "RooStats/HistFactory/MakeModelAndMeasurementsFast.h"
+#include "RooStats/HistFactory/Measurement.h"
+#include "RooStats/HistFactory/ParamHistFunc.h"
+#include "RooStats/HistFactory/HistFactoryModelUtils.h"
+#include "RooStats/HistFactory/RooBarlowBeestonLL.h"
+
+using namespace RooFit;
+using namespace RooStats;
+using namespace HistFactory;
+
+using std::string;
+using std::to_string;
+
+// arXiv:1409.4557v2
+# define TB0 1.5195 // (Table. 1)
+# define TBp 1.6384 // (Table. 1)
+# define BR_Kplus_nunubar 0.00000398 // (eq. 10)
+# define BR_K0star_nunubar 0.00000919 // (eq. 11)
+# define BR_K0_nunubar (BR_Kplus_nunubar*TB0/TBp) // under (eq. 15)
+# define BR_Kplusstar_nunubar (BR_K0star_nunubar*TBp/TB0) // under (eq. 15)
+# define BR_Xs_nunubar 0.000029 // (eq. 23)
+# define BR_Xsu_nonresonant_nunubar (BR_Xs_nunubar - BR_Kplus_nunubar - BR_Kplusstar_nunubar)
+# define BR_Xsd_nonresonant_nunubar (BR_Xs_nunubar - BR_K0_nunubar - BR_K0star_nunubar)
+
+// uncertainty of BR from parametric reason
+# define Sigma_BR_Kplus_nunubar 0.00000019
+# define Sigma_BR_K0star_nunubar 0.00000050
+# define Sigma_BR_K0_nunubar (Sigma_BR_Kplus_nunubar*TB0/TBp)
+# define Sigma_BR_Kplusstar_nunubar (Sigma_BR_K0star_nunubar*TBp/TB0)
+# define Sigma_BR_Xs_nunubar 0.000003
+
+// https://confluence.desy.de/pages/viewpage.action?pageId=107054222
+# define N_BpBp_1invab 565400000.0
+# define N_B0B0_1invab 534600000.0
+
+# define N_Kplus_nunubar_1invab (2.0 * N_BpBp_1invab * BR_Kplus_nunubar)
+# define N_Kplusstar_nunubar_1invab (2.0 * N_BpBp_1invab * BR_Kplusstar_nunubar)
+# define N_Xsu_nonresonant_nunubar_1invab (2.0 * N_BpBp_1invab * BR_Xsu_nonresonant_nunubar)
+# define N_K0_nunubar_1invab (2.0 * N_B0B0_1invab * BR_K0_nunubar)
+# define N_K0star_nunubar_1invab (2.0 * N_B0B0_1invab * BR_K0star_nunubar)
+# define N_Xsd_nunubar_1invab (2.0 * N_B0B0_1invab * BR_Xsd_nonresonant_nunubar)
+
+# define Lumi_validation_MC 0.3 // ab-1
+
+// my MC sample number
+# define N_Kplus_nunubar 10000000.0
+# define N_K0_nunubar 10000000.0
+# define N_Kplusstar_nunubar 10000000.0
+# define N_K0star_nunubar 10000000.0
+# define N_Xsu_nonresonant_nunubar 50000000.0
+# define N_Xsd_nonresonant_nunubar 50000000.0
+
+// scale factor for each MC sample
+# define Scale_Kplus (N_Kplus_nunubar_1invab/N_Kplus_nunubar)
+# define Scale_Kplusstar (N_Kplusstar_nunubar_1invab/N_Kplusstar_nunubar)
+# define Scale_Xsu_nonresonant (N_Xsu_nonresonant_nunubar_1invab/N_Xsu_nonresonant_nunubar)
+# define Scale_K0 (N_K0_nunubar_1invab/N_K0_nunubar)
+# define Scale_K0star (N_K0star_nunubar_1invab/N_K0star_nunubar)
+# define Scale_Xsd_nonresonant (N_Xsd_nunubar_1invab/N_Xsd_nonresonant_nunubar)
+
+# define KS0_rel_uncertainty 0.6 // %/cm
+# define track_rel_uncertainty 0.69 // %
+# define pi0_correction 0.932
+# define pi0_rel_uncertainty ((0.0369 / 0.932) * 100.0) // %
+# define Kaon_PID_max_uncertainty 0.1 // not percentage. relative uncertainty
+// https://indico.belle2.org/event/6872/contributions/37447/attachments/17127/25504/FEIperformance_B2GM.pdf
+# define FEI_cal_Bc 0.679
+# define FEI_cal_Bc_uncertainty (0.017/FEI_cal_Bc) // not percentage. relative uncertainty
+# define FEI_cal_B0 0.713
+# define FEI_cal_B0_uncertainty (0.019/FEI_cal_B0) // not percentage. relative uncertainty
+
+//# define Htransition_Xsu_change (-0.142)
+//# define Htransition_Xsd_change (-0.099)
+//# define Ltransition_Xsu_change (-0.002)
+//# define Ltransition_Xsd_change (0.067)
+//# define Hmb_Xsu_change (-0.067)
+//# define Hmb_Xsd_change (-0.058)
+//# define Lmb_Xsu_change (-0.062)
+//# define Lmb_Xsd_change (-0.046)
+//# define Hpf_Xsu_change (-0.120)
+//# define Hpf_Xsd_change (-0.073)
+//# define Lpf_Xsu_change (-0.014)
+//# define Lpf_Xsd_change (0.019)
+
+//# define FEI_cal_Bc 1.0
+//# define FEI_cal_Bc_uncertainty 0.0
+//# define FEI_cal_B0 1.0
+//# define FEI_cal_B0_uncertainty 0.0
+
+//# define Htransition_Xsu_change 0.0
+//# define Htransition_Xsd_change 0.0
+//# define Ltransition_Xsu_change 0.0
+//# define Ltransition_Xsd_change 0.0
+//# define Hmb_Xsu_change 0.0
+//# define Hmb_Xsd_change 0.0
+//# define Lmb_Xsu_change 0.0
+//# define Lmb_Xsd_change 0.0
+//# define Hpf_Xsu_change 0.0
+//# define Hpf_Xsd_change 0.0
+//# define Lpf_Xsu_change 0.0
+//# define Lpf_Xsd_change 0.0
+
+# define RarityBins 10
+
+int template_draw() {
+
+	const char* fname = "PDFandDATA_10.root";
+
+	TFile* f = new TFile(fname, "read");
+
+
+	TH1D* signal_hist = (TH1D*) f->Get("Signal_nominal");
+	TH1D* chg_hist = (TH1D*)f->Get("CHG_nominal");
+	TH1D* mix_hist = (TH1D*)f->Get("MIX_nominal");
+	TH1D* uubar_hist = (TH1D*)f->Get("UUBAR_nominal");
+	TH1D* ddbar_hist = (TH1D*)f->Get("DDBAR_nominal");
+	TH1D* ssbar_hist = (TH1D*)f->Get("SSBAR_nominal");
+	TH1D* charm_hist = (TH1D*)f->Get("CHARM_nominal");
+
+	TCanvas* c_temp = new TCanvas("c", "", 800, 800);
+	signal_hist->SetStats(false);
+	signal_hist->GetYaxis()->SetRangeUser(0., 2.3);
+	signal_hist->SetFillColor(kBlue + 1);
+	signal_hist->SetLineWidth(0);
+	signal_hist->Draw("hist");
+	c_temp->SaveAs("signal_hist.png");
+
+	THStack* Stack = new THStack("thstack", ";transformed FBDT_{1};number of candidates");
+
+	chg_hist->SetFillColor(kRed + 1);
+	chg_hist->SetLineWidth(0);
+
+	mix_hist->SetFillColor(kOrange + 1);
+	mix_hist->SetLineWidth(0);
+
+	uubar_hist->SetFillColor(kGreen + 1);
+	uubar_hist->SetLineWidth(0);
+
+	ddbar_hist->SetFillColor(kAzure + 1);
+	ddbar_hist->SetLineWidth(0);
+
+	ssbar_hist->SetFillColor(kViolet + 1);
+	ssbar_hist->SetLineWidth(0);
+
+	charm_hist->SetFillColor(kPink + 1);
+	charm_hist->SetLineWidth(0);
+
+	Stack->Add(chg_hist);
+	Stack->Add(mix_hist);
+	Stack->Add(uubar_hist);
+	Stack->Add(ddbar_hist);
+	Stack->Add(ssbar_hist);
+	Stack->Add(charm_hist);
+
+	Stack->Draw("hist");
+
+	TLegend* legend = gPad->BuildLegend(0.9, 0.9, 0.7, 0.7);
+	legend->SetFillStyle(0);
+	legend->SetLineWidth(0);
+
+	c_temp->SaveAs("bkgs_hist.png");
+
+	return 0;
+}
