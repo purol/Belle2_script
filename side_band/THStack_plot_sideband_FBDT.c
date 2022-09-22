7520f42e25495658fea985fec784fa427c741821
purol <purol0812@gmail.com>
2022-09-22 21:06:41 +0900

    bug fix

--- side_band/THStack_plot_sideband_FBDT.c
+++ side_band/THStack_plot_sideband_FBDT.c
@@ -677,7 +677,7 @@ void THStack_plot_sideband_FBDT() {
     for (int i = 0; i < (int)Sideband_MC_values[0].size(); i++) MC_sum = MC_sum + weights.at(i);
     printf("data num: %ld\n", Sideband_data_values[0].size());
     printf("MC num with calibration: %lf\n", MC_sum);
-    printf("MC with calibration: %lf +- %lf\n", stat_error_hist[0]->GetBinContent, stat_error_hist[0]->GetBinError(0));
-    printf("data with calibration: %lf +- %lf\n", data_hist[0]->GetBinContent, data_hist[0]->GetBinError(0));
-    printf("MC/data with calibration: %lf +- %lf\n", Ratio_one_bin[0]->GetBinContent, Ratio_one_bin[0]->GetBinError(0));
+    printf("MC with calibration: %lf +- %lf\n", stat_error_hist[0]->GetBinContent(0), stat_error_hist[0]->GetBinError(0));
+    printf("data with calibration: %lf +- %lf\n", data_hist[0]->GetBinContent(0), data_hist[0]->GetBinError(0));
+    printf("MC/data with calibration: %lf +- %lf\n", Ratio_one_bin[0]->GetBinContent(0), Ratio_one_bin[0]->GetBinError(0));
 }
