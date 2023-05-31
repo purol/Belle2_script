97943f7deec7e45b4ec3736dcb4e7de8d4dc05e6
unknown <purol0812@gmail.com>
2023-05-31 12:08:22 +0900

    bug fix

--- HistFactory/WorkSpace.C
+++ HistFactory/WorkSpace.C
@@ -169,13 +169,13 @@ int ReadNPIDEigenVector(const char* dirname) {
 int ReadNBREigenVector(const char* dirname) {
 	int Nentry = 0; // number of eigen values/vectors
 	double eigen_value = 0; // eigen value
-	double weight_sys[RarityBins * 2] = { 0.0 }; // eigen vector
+	double weight_sys[RarityBins * 3] = { 0.0 }; // eigen vector
 
 	FILE* fp;
 	fp = fopen(dirname, "r");
 	while (true) {
 		if (fscanf(fp, "%lf\n", &eigen_value) == EOF) break;
-		for (int i = 0; i < RarityBins * 2; i++) {
+		for (int i = 0; i < RarityBins * 3; i++) {
 			if (fscanf(fp, "%lf\n", &weight_sys[i]) == EOF) break;
 		}
 		Nentry++;
