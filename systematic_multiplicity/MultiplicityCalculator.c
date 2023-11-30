207994d8b9226800eafc19cb001bc72da4ba00f1
unknown <purol0812@gmail.com>
2023-11-30 13:27:53 +0900

    bug fix for FEI corrector

--- systematic_multiplicity/MultiplicityCalculator.c
+++ systematic_multiplicity/MultiplicityCalculator.c
@@ -208,11 +208,11 @@ double Corrector_FEI::GetFEICalFactor(double UpsilonID, double BtagID, std::stri
 double Corrector_FEI::GetFEICalFactor(int index, bool IsItCharged, std::string type) {
     if (type == "MC15ri") {
         if (IsItCharged) return FEI_cal_Bc_MC15ri[index];
-        else FEI_cal_B0_MC15ri[index];
+        else return FEI_cal_B0_MC15ri[index];
     }
     else if (type == "MC15rd") {
         if (IsItCharged) return FEI_cal_Bc_MC15rd[index];
-        else FEI_cal_B0_MC15rd[index];
+        else return FEI_cal_B0_MC15rd[index];
     }
     else {
         printf("[Corrector_FEI] Invalid type!\n");
@@ -266,11 +266,11 @@ double Corrector_FEI::GetFEICalFactorUncer(double UpsilonID, double BtagID, std:
 double Corrector_FEI::GetmodeID(int index, bool IsItCharged, std::string type) {
     if (type == "MC15ri") {
         if (IsItCharged) return FEI_cal_Bc_modeID_MC15ri[index];
-        else FEI_cal_B0_modeID_MC15ri[index];
+        else return FEI_cal_B0_modeID_MC15ri[index];
     }
     else if (type == "MC15rd") {
         if (IsItCharged) return FEI_cal_Bc_modeID_MC15rd[index];
-        else FEI_cal_B0_modeID_MC15rd[index];
+        else return FEI_cal_B0_modeID_MC15rd[index];
     }
     else {
         printf("[Corrector_FEI] Invalid type!\n");
