a1500a653db233315f8efebcd1bc63c23964a818
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-02-17 13:42:18 +0900

    bug fix

--- bsub_KEK/general/template.h
+++ bsub_KEK/general/template.h
@@ -29,7 +29,7 @@ void FillTemplate(TH1D* hist, double FBDToutput, double total_weight, double MXs
 	if ((MXs > 0.0) && (MXs < 0.6)) { // MXs1
 		if ((FBDToutput >= BinMIN_MX1) && (FBDToutput <= BinMAX_MX1)) {
 			double binwidth = (BinMAX_MX1 - BinMIN_MX1) / RarityBins_MX1;
-			double binindex = std::ceil((FBDToutput - BinMIN_MX1) / binwidth); // start from 0. global bin index
+			double binindex = std::floor((FBDToutput - BinMIN_MX1) / binwidth); // start from 0. global bin index
 			if (binindex >= RarityBins_MX1) {
 				printf("Bin index is larger than expected. Just put maximum value.\n");
 				binindex = RarityBins_MX1 - 1.0;
@@ -48,7 +48,7 @@ void FillTemplate(TH1D* hist, double FBDToutput, double total_weight, double MXs
 	else if ((MXs >= 0.6) && (MXs < 1.0)) { // MX2
 		if ((FBDToutput >= BinMIN_MX2) && (FBDToutput <= BinMAX_MX2)) {
 			double binwidth = (BinMAX_MX2 - BinMIN_MX2) / RarityBins_MX2;
-			double binindex = RarityBins_MX1 + std::ceil((FBDToutput - BinMIN_MX2) / binwidth); // start from RarityBins_MX1. global bin index
+			double binindex = RarityBins_MX1 + std::floor((FBDToutput - BinMIN_MX2) / binwidth); // start from RarityBins_MX1. global bin index
 			if (binindex >= (RarityBins_MX1 + RarityBins_MX2)) {
 				printf("Bin index is larger than expected. Just put maximum value.\n");
 				binindex = RarityBins_MX1 + RarityBins_MX2 - 1.0;
@@ -67,7 +67,7 @@ void FillTemplate(TH1D* hist, double FBDToutput, double total_weight, double MXs
 	else if ((MXs >= 1.0) && (MXs < 2.0)) { // MX3
 		if ((FBDToutput >= BinMIN_MX3) && (FBDToutput <= BinMAX_MX3)) {
 			double binwidth = (BinMAX_MX3 - BinMIN_MX3) / RarityBins_MX3;
-			double binindex = RarityBins_MX1 + RarityBins_MX2 + std::ceil((FBDToutput - BinMIN_MX3) / binwidth); // start from RarityBins_MX1 + RarityBins_MX2. global bin index
+			double binindex = RarityBins_MX1 + RarityBins_MX2 + std::floor((FBDToutput - BinMIN_MX3) / binwidth); // start from RarityBins_MX1 + RarityBins_MX2. global bin index
 			if (binindex >= (RarityBins_MX1 + RarityBins_MX2 + RarityBins_MX3)) {
 				printf("Bin index is larger than expected. Just put maximum value.\n");
 				binindex = RarityBins_MX1 + RarityBins_MX2 + RarityBins_MX3 - 1.0;
