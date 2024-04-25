c8cbe9c4056b55ebac5c8444904fe9b9baca020d
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-04-25 16:32:32 +0900

    able to use unequal bin

--- NEW/include/template.h
+++ NEW/include/template.h
@@ -5,19 +5,22 @@
 #include <cmath>
 
 // 0.0 < MXs < 0.6
-# define RarityBins_MX1 6
-# define BinMIN_MX1 0.96
+# define RarityBins_MX1 5
+# define BinMIN_MX1 0.90
 # define BinMAX_MX1 1.0
+const double boundary_MXs1[RarityBins_MX1 + 1] = { BinMIN_MX1, 0.9395, 0.979, 0.986, 0.993, BinMAX_MX1 };
 
 // 0.6 < MXs < 1.0
-# define RarityBins_MX2 6
-# define BinMIN_MX2 0.96
+# define RarityBins_MX2 4
+# define BinMIN_MX2 0.90
 # define BinMAX_MX2 1.0
+const double boundary_MXs2[RarityBins_MX2 + 1] = { BinMIN_MX2, 0.925, 0.95, 0.975, BinMAX_MX2 };
 
 // 1.0 < MXs < 2.0
-# define RarityBins_MX3 6
-# define BinMIN_MX3 0.96
+# define RarityBins_MX3 5
+# define BinMIN_MX3 0.90
 # define BinMAX_MX3 1.0
+const double boundary_MXs2[RarityBins_MX3 + 1] = { BinMIN_MX3, 0.92, 0.94, 0.96, 0.98, BinMAX_MX3 };
 
 const int RarityBins = (RarityBins_MX1 + RarityBins_MX2 + RarityBins_MX3);
 const int BinMIN = 0;
@@ -25,21 +28,12 @@ const int BinMAX = RarityBins;
 
 // TH1D* hist = new TH1D("histogram", ";;", RarityBins, BinMIN, BinMAX);
 
-double FillTemplate(TH1D* hist, double FBDToutput, double total_weight, double MXs) {
+double ReturnBinIndex(double FBDToutput, double MXs) {
 	if ((MXs > 0.0) && (MXs < 0.6)) { // MXs1
-		if ((FBDToutput >= BinMIN_MX1) && (FBDToutput <= BinMAX_MX1)) {
-			double binwidth = (BinMAX_MX1 - BinMIN_MX1) / RarityBins_MX1;
-			double binindex = std::floor((FBDToutput - BinMIN_MX1) / binwidth); // start from 0. global bin index
-			if (binindex >= RarityBins_MX1) {
-				printf("Bin index is larger than expected. Just put maximum value.\n");
-				binindex = RarityBins_MX1 - 1.0;
+		if ((FBDToutput > BinMIN_MX1) && (FBDToutput <= BinMAX_MX1)) {
+			for (int i = 0; i < RarityBins_MX1; i++) {
+				if ((FBDToutput > boundary_MXs1[i]) && (FBDToutput <= boundary_MXs1[i + 1])) return ((double)(i) + 0.5);
 			}
-			else if (binindex < 0) {
-				printf("Bin index is smaller than expected. Just put minimum value.\n");
-				binindex = 0.0;
-			}
-			hist->Fill(binindex + 0.5, total_weight); // add 0.5 to avoid unexpected rounding error
-			return total_weight;
 		}
 		else {
 			printf("FBDT output is out of range!\n");
@@ -48,18 +42,9 @@ double FillTemplate(TH1D* hist, double FBDToutput, double total_weight, double M
 	}
 	else if ((MXs >= 0.6) && (MXs < 1.0)) { // MX2
 		if ((FBDToutput >= BinMIN_MX2) && (FBDToutput <= BinMAX_MX2)) {
-			double binwidth = (BinMAX_MX2 - BinMIN_MX2) / RarityBins_MX2;
-			double binindex = RarityBins_MX1 + std::floor((FBDToutput - BinMIN_MX2) / binwidth); // start from RarityBins_MX1. global bin index
-			if (binindex >= (RarityBins_MX1 + RarityBins_MX2)) {
-				printf("Bin index is larger than expected. Just put maximum value.\n");
-				binindex = RarityBins_MX1 + RarityBins_MX2 - 1.0;
+			for (int i = 0; i < RarityBins_MX2; i++) {
+				if ((FBDToutput > boundary_MXs2[i]) && (FBDToutput <= boundary_MXs2[i + 1])) return ((double)(i)+0.5);
 			}
-			else if (binindex < 0) {
-				printf("Bin index is smaller than expected. Just put minimum value.\n");
-				binindex = RarityBins_MX1;
-			}
-			hist->Fill(binindex + 0.5, total_weight); // add 0.5 to avoid unexpected rounding error
-			return total_weight;
 		}
 		else {
 			printf("FBDT output is out of range!\n");
@@ -68,18 +53,9 @@ double FillTemplate(TH1D* hist, double FBDToutput, double total_weight, double M
 	}
 	else if ((MXs >= 1.0) && (MXs < 2.0)) { // MX3
 		if ((FBDToutput >= BinMIN_MX3) && (FBDToutput <= BinMAX_MX3)) {
-			double binwidth = (BinMAX_MX3 - BinMIN_MX3) / RarityBins_MX3;
-			double binindex = RarityBins_MX1 + RarityBins_MX2 + std::floor((FBDToutput - BinMIN_MX3) / binwidth); // start from RarityBins_MX1 + RarityBins_MX2. global bin index
-			if (binindex >= (RarityBins_MX1 + RarityBins_MX2 + RarityBins_MX3)) {
-				printf("Bin index is larger than expected. Just put maximum value.\n");
-				binindex = RarityBins_MX1 + RarityBins_MX2 + RarityBins_MX3 - 1.0;
+			for (int i = 0; i < RarityBins_MX3; i++) {
+				if ((FBDToutput > boundary_MXs3[i]) && (FBDToutput <= boundary_MXs3[i + 1])) return ((double)(i)+0.5);
 			}
-			else if (binindex < 0) {
-				printf("Bin index is smaller than expected. Just put minimum value.\n");
-				binindex = RarityBins_MX1 + RarityBins_MX2;
-			}
-			hist->Fill(binindex + 0.5, total_weight); // add 0.5 to avoid unexpected rounding error
-			return total_weight;
 		}
 		else {
 			printf("FBDT output is out of range!\n");
@@ -90,134 +66,17 @@ double FillTemplate(TH1D* hist, double FBDToutput, double total_weight, double M
 		printf("MXs is out of range!\n");
 		exit(1);
 	}
+}
+
+double FillTemplate(TH1D* hist, double FBDToutput, double total_weight, double MXs) {
+	double binindex = ReturnBinIndex(FBDToutput, MXs);
+	hist->Fill(binindex + 0.5, total_weight); // add 0.5 to avoid unexpected rounding error
+	return total_weight;
 }
 
 double DoesNotFillTemplate(double FBDToutput, double total_weight, double MXs) {
-	if ((MXs > 0.0) && (MXs < 0.6)) { // MXs1
-		if ((FBDToutput >= BinMIN_MX1) && (FBDToutput <= BinMAX_MX1)) {
-			double binwidth = (BinMAX_MX1 - BinMIN_MX1) / RarityBins_MX1;
-			double binindex = std::floor((FBDToutput - BinMIN_MX1) / binwidth); // start from 0. global bin index
-			if (binindex >= RarityBins_MX1) {
-				printf("Bin index is larger than expected. Just put maximum value.\n");
-				binindex = RarityBins_MX1 - 1.0;
-			}
-			else if (binindex < 0) {
-				printf("Bin index is smaller than expected. Just put minimum value.\n");
-				binindex = 0.0;
-			}
-			return total_weight;
-		}
-		else {
-			printf("FBDT output is out of range!\n");
-			exit(1);
-		}
-	}
-	else if ((MXs >= 0.6) && (MXs < 1.0)) { // MX2
-		if ((FBDToutput >= BinMIN_MX2) && (FBDToutput <= BinMAX_MX2)) {
-			double binwidth = (BinMAX_MX2 - BinMIN_MX2) / RarityBins_MX2;
-			double binindex = RarityBins_MX1 + std::floor((FBDToutput - BinMIN_MX2) / binwidth); // start from RarityBins_MX1. global bin index
-			if (binindex >= (RarityBins_MX1 + RarityBins_MX2)) {
-				printf("Bin index is larger than expected. Just put maximum value.\n");
-				binindex = RarityBins_MX1 + RarityBins_MX2 - 1.0;
-			}
-			else if (binindex < 0) {
-				printf("Bin index is smaller than expected. Just put minimum value.\n");
-				binindex = RarityBins_MX1;
-			}
-			return total_weight;
-		}
-		else {
-			printf("FBDT output is out of range!\n");
-			exit(1);
-		}
-	}
-	else if ((MXs >= 1.0) && (MXs < 2.0)) { // MX3
-		if ((FBDToutput >= BinMIN_MX3) && (FBDToutput <= BinMAX_MX3)) {
-			double binwidth = (BinMAX_MX3 - BinMIN_MX3) / RarityBins_MX3;
-			double binindex = RarityBins_MX1 + RarityBins_MX2 + std::floor((FBDToutput - BinMIN_MX3) / binwidth); // start from RarityBins_MX1 + RarityBins_MX2. global bin index
-			if (binindex >= (RarityBins_MX1 + RarityBins_MX2 + RarityBins_MX3)) {
-				printf("Bin index is larger than expected. Just put maximum value.\n");
-				binindex = RarityBins_MX1 + RarityBins_MX2 + RarityBins_MX3 - 1.0;
-			}
-			else if (binindex < 0) {
-				printf("Bin index is smaller than expected. Just put minimum value.\n");
-				binindex = RarityBins_MX1 + RarityBins_MX2;
-			}
-			return total_weight;
-		}
-		else {
-			printf("FBDT output is out of range!\n");
-			exit(1);
-		}
-	}
-	else {
-		printf("MXs is out of range!\n");
-		exit(1);
-	}
-}
-
-double ReturnBinIndex(double FBDToutput, double MXs) {
-	if ((MXs > 0.0) && (MXs < 0.6)) { // MXs1
-		if ((FBDToutput >= BinMIN_MX1) && (FBDToutput <= BinMAX_MX1)) {
-			double binwidth = (BinMAX_MX1 - BinMIN_MX1) / RarityBins_MX1;
-			double binindex = std::floor((FBDToutput - BinMIN_MX1) / binwidth); // start from 0. global bin index
-			if (binindex >= RarityBins_MX1) {
-				printf("Bin index is larger than expected. Just put maximum value.\n");
-				binindex = RarityBins_MX1 - 1.0;
-			}
-			else if (binindex < 0) {
-				printf("Bin index is smaller than expected. Just put minimum value.\n");
-				binindex = 0.0;
-			}
-			return (binindex + 0.5); // add 0.5 to avoid unexpected rounding error
-		}
-		else {
-			printf("FBDT output is out of range!\n");
-			exit(1);
-		}
-	}
-	else if ((MXs >= 0.6) && (MXs < 1.0)) { // MX2
-		if ((FBDToutput >= BinMIN_MX2) && (FBDToutput <= BinMAX_MX2)) {
-			double binwidth = (BinMAX_MX2 - BinMIN_MX2) / RarityBins_MX2;
-			double binindex = RarityBins_MX1 + std::floor((FBDToutput - BinMIN_MX2) / binwidth); // start from RarityBins_MX1. global bin index
-			if (binindex >= (RarityBins_MX1 + RarityBins_MX2)) {
-				printf("Bin index is larger than expected. Just put maximum value.\n");
-				binindex = RarityBins_MX1 + RarityBins_MX2 - 1.0;
-			}
-			else if (binindex < 0) {
-				printf("Bin index is smaller than expected. Just put minimum value.\n");
-				binindex = RarityBins_MX1;
-			}
-			return (binindex + 0.5); // add 0.5 to avoid unexpected rounding error
-		}
-		else {
-			printf("FBDT output is out of range!\n");
-			exit(1);
-		}
-	}
-	else if ((MXs >= 1.0) && (MXs < 2.0)) { // MX3
-		if ((FBDToutput >= BinMIN_MX3) && (FBDToutput <= BinMAX_MX3)) {
-			double binwidth = (BinMAX_MX3 - BinMIN_MX3) / RarityBins_MX3;
-			double binindex = RarityBins_MX1 + RarityBins_MX2 + std::floor((FBDToutput - BinMIN_MX3) / binwidth); // start from RarityBins_MX1 + RarityBins_MX2. global bin index
-			if (binindex >= (RarityBins_MX1 + RarityBins_MX2 + RarityBins_MX3)) {
-				printf("Bin index is larger than expected. Just put maximum value.\n");
-				binindex = RarityBins_MX1 + RarityBins_MX2 + RarityBins_MX3 - 1.0;
-			}
-			else if (binindex < 0) {
-				printf("Bin index is smaller than expected. Just put minimum value.\n");
-				binindex = RarityBins_MX1 + RarityBins_MX2;
-			}
-			return (binindex + 0.5); // add 0.5 to avoid unexpected rounding error
-		}
-		else {
-			printf("FBDT output is out of range!\n");
-			exit(1);
-		}
-	}
-	else {
-		printf("MXs is out of range!\n");
-		exit(1);
-	}
+	double binindex = ReturnBinIndex(FBDToutput, MXs);
+	return total_weight;
 }
 
 #endif 
