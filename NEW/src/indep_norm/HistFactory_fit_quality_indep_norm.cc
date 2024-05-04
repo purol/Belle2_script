8b2c90d33c565e0df739e5491a8aef8ef9e36416
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-05-04 17:51:59 +0900

    set default print level to be 2

--- NEW/src/indep_norm/HistFactory_fit_quality_indep_norm.cc
+++ NEW/src/indep_norm/HistFactory_fit_quality_indep_norm.cc
@@ -568,6 +568,7 @@ void Debug(RooWorkspace* w, RooFitResult* fitres, RooDataSet* data) {
 
 int main(int argc, char* argv[]) {
 
+    ::ROOT::Math::MinimizerOptions::SetDefaultPrintLevel(2);
     ::ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2", "Minimize"); // default: Minuit Migrad
     ::ROOT::Math::MinimizerOptions::SetDefaultStrategy(1); // default 1
 
