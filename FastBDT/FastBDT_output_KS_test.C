66058ca466c4d4aafdd5a09d4f5fe3b887d2079b
Junewoo Park <junewoo@g.ecc.u-tokyo.ac.jp>
2024-08-17 13:22:39 +0900

    use header for FBDT plot maker

--- FastBDT/FastBDT_output_KS_test.C
+++ FastBDT/FastBDT_output_KS_test.C
@@ -9,89 +9,7 @@
 #include "RooPlot.h"
 using namespace RooFit ;
 
-// arXiv:1409.4557v2, PhysRevD.107.014511
-# define TB0 1.5195 // (Table. 1)
-# define TBp 1.6384 // (Table. 1)
-# define BR_Kplus_nunubar 0.000005044 // Table VI = (5.044 +- 0.402) * 10^{-6}
-# define BR_K0star_nunubar 0.00000919 // (eq. 11)
-# define BR_K0_nunubar (BR_Kplus_nunubar*TB0/TBp) // under (eq. 15). In Table VI = (4.6669 +- 0.3707) * 10^{-6}
-# define BR_Kplusstar_nunubar (BR_K0star_nunubar*TBp/TB0) // under (eq. 15)
-# define BR_Xs_nunubar 0.000029 // (eq. 23)
-# define BR_Xsu_nonresonant_nunubar (BR_Xs_nunubar - BR_Kplus_nunubar - BR_Kplusstar_nunubar)
-# define BR_Xsd_nonresonant_nunubar (BR_Xs_nunubar - BR_K0_nunubar - BR_K0star_nunubar)
-
-// according to DIRAC
-# define N_BpBp_1invab 540000000.0
-# define N_B0B0_1invab 510000000.0
-
-# define BR_BpBp 0.514
-# define BR_B0B0 0.486
-
-// https://confluence.desy.de/pages/viewpage.action?spaceKey=BI&title=Conference+readiness
-# define N_BB_LS1 387100000.0 // NBB = (387.1 +/- 5.6) x 10^6
-
-# define N_Kplus_nunubar_LS1 (2.0 * N_BB_LS1 * (BR_BpBp/(BR_BpBp+BR_B0B0)) * BR_Kplus_nunubar)
-# define N_Kplusstar_nunubar_LS1 (2.0 * N_BB_LS1 * (BR_BpBp/(BR_BpBp+BR_B0B0)) * BR_Kplusstar_nunubar)
-# define N_Xsu_nonresonant_nunubar_LS1 (2.0 * N_BB_LS1 * (BR_BpBp/(BR_BpBp+BR_B0B0)) * BR_Xsu_nonresonant_nunubar)
-# define N_K0_nunubar_LS1 (2.0 * N_BB_LS1 * (BR_B0B0/(BR_BpBp+BR_B0B0)) * BR_K0_nunubar)
-# define N_K0star_nunubar_LS1 (2.0 * N_BB_LS1 * (BR_B0B0/(BR_BpBp+BR_B0B0)) * BR_K0star_nunubar)
-# define N_Xsd_nunubar_LS1 (2.0 * N_BB_LS1 * (BR_B0B0/(BR_BpBp+BR_B0B0)) * BR_Xsd_nonresonant_nunubar)
-
-// SIGNAL MC sample number befor skimming
-# define N_Kplus_train 7039000.0
-# define N_K0_train 7166624.0
-# define N_Kplusstar_train 7039000.0
-# define N_K0star_train 7166624.0
-# define N_Xsu_nonresonant_train 35195000.0
-# define N_Xsd_nonresonant_train 34940430.0
-# define N_Kplus_test 2961000.0
-# define N_K0_test 2833376.0
-# define N_Kplusstar_test 2961000.0
-# define N_K0star_test 2833376.0
-# define N_Xsu_nonresonant_test 14805000.0
-# define N_Xsd_nonresonant_test 15059570.0
-
-// scale factor for SIGNAL MC sample until LS1
-# define Scale_Kplus_train (N_Kplus_nunubar_LS1/N_Kplus_train)
-# define Scale_Kplusstar_train (N_Kplusstar_nunubar_LS1/N_Kplusstar_train)
-# define Scale_Xsu_nonresonant_train (N_Xsu_nonresonant_nunubar_LS1/N_Xsu_nonresonant_train)
-# define Scale_K0_train (N_K0_nunubar_LS1/N_K0_train)
-# define Scale_K0star_train (N_K0star_nunubar_LS1/N_K0star_train)
-# define Scale_Xsd_nonresonant_train (N_Xsd_nunubar_LS1/N_Xsd_nonresonant_train)
-# define Scale_Kplus_test (N_Kplus_nunubar_LS1/N_Kplus_test)
-# define Scale_Kplusstar_test (N_Kplusstar_nunubar_LS1/N_Kplusstar_test)
-# define Scale_Xsu_nonresonant_test (N_Xsu_nonresonant_nunubar_LS1/N_Xsu_nonresonant_test)
-# define Scale_K0_test (N_K0_nunubar_LS1/N_K0_test)
-# define Scale_K0star_test (N_K0star_nunubar_LS1/N_K0star_test)
-# define Scale_Xsd_nonresonant_test (N_Xsd_nunubar_LS1/N_Xsd_nonresonant_test)
-
-// BKG MC sample number (2.8/ab for BB, 1.0/ab for qq)
-# define N_CHG_test 139768443.0
-# define N_MIX_test 104591690.0
-# define N_UUBAR_test 107641351.0
-# define N_DDBAR_test 25588403.0
-# define N_SSBAR_test 20668794.0
-# define N_CHARM_test 116108850.0
-# define N_CHG_train 151915459.0
-# define N_MIX_train 120136353.0
-# define N_UUBAR_train 158509639.0
-# define N_DDBAR_train 38644413.0
-# define N_SSBAR_train 31001866.0
-# define N_CHARM_train 174901296.0
-
-// new scale factor for BKG MC sample with additional 1/ab (364.436 - 2.763 = 361.673/fb), until LS1
-# define Scale_CHG_train ((N_BB_LS1* (BR_BpBp / (BR_BpBp + BR_B0B0))) / (2.8 * N_BpBp_1invab * (N_CHG_train / (N_CHG_train + N_CHG_test)) ))
-# define Scale_MIX_train ((N_BB_LS1* (BR_B0B0 / (BR_BpBp + BR_B0B0))) / (2.8 * N_B0B0_1invab * (N_MIX_train / (N_MIX_train + N_MIX_test)) ))
-# define Scale_UUBAR_train (0.361673/((N_UUBAR_train/(N_UUBAR_train + N_UUBAR_test))*1.0))
-# define Scale_DDBAR_train (0.361673/((N_DDBAR_train/(N_DDBAR_train + N_DDBAR_test))*1.0))
-# define Scale_SSBAR_train (0.361673/((N_SSBAR_train/(N_SSBAR_train + N_SSBAR_test))*1.0))
-# define Scale_CHARM_train (0.361673/((N_CHARM_train/(N_CHARM_train + N_CHARM_test))*1.0))
-# define Scale_CHG_test ((N_BB_LS1* (BR_BpBp / (BR_BpBp + BR_B0B0))) / (2.8 * N_BpBp_1invab * (N_CHG_test / (N_CHG_train + N_CHG_test)) ))
-# define Scale_MIX_test ((N_BB_LS1* (BR_B0B0 / (BR_BpBp + BR_B0B0))) / (2.8 * N_B0B0_1invab * (N_MIX_test / (N_MIX_train + N_MIX_test)) ))
-# define Scale_UUBAR_test (0.361673/((N_UUBAR_test/(N_UUBAR_train + N_UUBAR_test))*1.0))
-# define Scale_DDBAR_test (0.361673/((N_DDBAR_test/(N_DDBAR_train + N_DDBAR_test))*1.0))
-# define Scale_SSBAR_test (0.361673/((N_SSBAR_test/(N_SSBAR_train + N_SSBAR_test))*1.0))
-# define Scale_CHARM_test (0.361673/((N_CHARM_test/(N_CHARM_train + N_CHARM_test))*1.0))
+#include "constants.h"
 
 void load_files(const char* dirname, std::vector<string>* names) {
     TSystemDirectory dir(dirname, dirname);
@@ -164,12 +82,12 @@ void FastBDT_output_KS_test()
 {
 
     // get data from root files
-    const char* train_dirname_Knunu = "./temp_v000_train/B2Knunu";
-    const char* train_dirname_Kstarnunu = "./temp_v000_train/B2Kstarnunu";
-    const char* train_dirname_Xsununu = "./temp_v000_train/B2Xsnunu";
-    const char* train_dirname_K0nunu = "./temp_v000_train/B02K0nunu";
-    const char* train_dirname_K0starnunu = "./temp_v000_train/B02K0starnunu";
-    const char* train_dirname_Xsdnunu = "./temp_v000_train/B02Xsnunu";
+    const char* train_dirname_Knunu = "./temp_v008_train/B2Knunu";
+    const char* train_dirname_Kstarnunu = "./temp_v008_train/B2Kstarnunu";
+    const char* train_dirname_Xsununu = "./temp_v008_train/B2Xsnunu";
+    const char* train_dirname_K0nunu = "./temp_v008_train/B02K0nunu";
+    const char* train_dirname_K0starnunu = "./temp_v008_train/B02K0starnunu";
+    const char* train_dirname_Xsdnunu = "./temp_v008_train/B02Xsnunu";
     LetsFill(train_dirname_Knunu, OBB_Oqq_SIGNAL_train, OBB_SIGNAL_train, Oqq_SIGNAL_train, Scale_Kplus_train);
     LetsFill(train_dirname_Kstarnunu, OBB_Oqq_SIGNAL_train, OBB_SIGNAL_train, Oqq_SIGNAL_train, Scale_Kplusstar_train);
     LetsFill(train_dirname_Xsununu, OBB_Oqq_SIGNAL_train, OBB_SIGNAL_train, Oqq_SIGNAL_train, Scale_Xsu_nonresonant_train);
@@ -177,12 +95,12 @@ void FastBDT_output_KS_test()
     LetsFill(train_dirname_K0starnunu, OBB_Oqq_SIGNAL_train, OBB_SIGNAL_train, Oqq_SIGNAL_train, Scale_K0star_train);
     LetsFill(train_dirname_Xsdnunu, OBB_Oqq_SIGNAL_train, OBB_SIGNAL_train, Oqq_SIGNAL_train, Scale_Xsd_nonresonant_train);
 
-    const char* train_dirname_CHG = "./temp_v000_train/CHG";
-    const char* train_dirname_MIX = "./temp_v000_train/MIX";
-    const char* train_dirname_UUBAR = "./temp_v000_train/UUBAR";
-    const char* train_dirname_DDBAR = "./temp_v000_train/DDBAR";
-    const char* train_dirname_SSBAR = "./temp_v000_train/SSBAR";
-    const char* train_dirname_CHARM = "./temp_v000_train/CHARM";
+    const char* train_dirname_CHG = "./temp_v008_train/CHG";
+    const char* train_dirname_MIX = "./temp_v008_train/MIX";
+    const char* train_dirname_UUBAR = "./temp_v008_train/UUBAR";
+    const char* train_dirname_DDBAR = "./temp_v008_train/DDBAR";
+    const char* train_dirname_SSBAR = "./temp_v008_train/SSBAR";
+    const char* train_dirname_CHARM = "./temp_v008_train/CHARM";
     LetsFill(train_dirname_CHG, OBB_Oqq_BKG_train, OBB_BKG_train, Oqq_BKG_train, Scale_CHG_train);
     LetsFill(train_dirname_MIX, OBB_Oqq_BKG_train, OBB_BKG_train, Oqq_BKG_train, Scale_MIX_train);
     LetsFill(train_dirname_UUBAR, OBB_Oqq_BKG_train, OBB_BKG_train, Oqq_BKG_train, Scale_UUBAR_train);
@@ -190,12 +108,12 @@ void FastBDT_output_KS_test()
     LetsFill(train_dirname_SSBAR, OBB_Oqq_BKG_train, OBB_BKG_train, Oqq_BKG_train, Scale_SSBAR_train);
     LetsFill(train_dirname_CHARM, OBB_Oqq_BKG_train, OBB_BKG_train, Oqq_BKG_train, Scale_CHARM_train);
 
-    const char* test_dirname_Knunu = "./temp_v000/B2Knunu";
-    const char* test_dirname_Kstarnunu = "./temp_v000/B2Kstarnunu";
-    const char* test_dirname_Xsununu = "./temp_v000/B2Xsnunu";
-    const char* test_dirname_K0nunu = "./temp_v000/B02K0nunu";
-    const char* test_dirname_K0starnunu = "./temp_v000/B02K0starnunu";
-    const char* test_dirname_Xsdnunu = "./temp_v000/B02Xsnunu";
+    const char* test_dirname_Knunu = "./temp_v008/B2Knunu";
+    const char* test_dirname_Kstarnunu = "./temp_v008/B2Kstarnunu";
+    const char* test_dirname_Xsununu = "./temp_v008/B2Xsnunu";
+    const char* test_dirname_K0nunu = "./temp_v008/B02K0nunu";
+    const char* test_dirname_K0starnunu = "./temp_v008/B02K0starnunu";
+    const char* test_dirname_Xsdnunu = "./temp_v008/B02Xsnunu";
     LetsFill(test_dirname_Knunu, OBB_Oqq_SIGNAL_test, OBB_SIGNAL_test, Oqq_SIGNAL_test, Scale_Kplus_test);
     LetsFill(test_dirname_Kstarnunu, OBB_Oqq_SIGNAL_test, OBB_SIGNAL_test, Oqq_SIGNAL_test, Scale_Kplusstar_test);
     LetsFill(test_dirname_Xsununu, OBB_Oqq_SIGNAL_test, OBB_SIGNAL_test, Oqq_SIGNAL_test, Scale_Xsu_nonresonant_test);
@@ -203,12 +121,12 @@ void FastBDT_output_KS_test()
     LetsFill(test_dirname_K0starnunu, OBB_Oqq_SIGNAL_test, OBB_SIGNAL_test, Oqq_SIGNAL_test, Scale_K0star_test);
     LetsFill(test_dirname_Xsdnunu, OBB_Oqq_SIGNAL_test, OBB_SIGNAL_test, Oqq_SIGNAL_test, Scale_Xsd_nonresonant_test);
 
-    const char* test_dirname_CHG = "./temp_v000/CHG";
-    const char* test_dirname_MIX = "./temp_v000/MIX";
-    const char* test_dirname_UUBAR = "./temp_v000/UUBAR";
-    const char* test_dirname_DDBAR = "./temp_v000/DDBAR";
-    const char* test_dirname_SSBAR = "./temp_v000/SSBAR";
-    const char* test_dirname_CHARM = "./temp_v000/CHARM";
+    const char* test_dirname_CHG = "./temp_v008/CHG";
+    const char* test_dirname_MIX = "./temp_v008/MIX";
+    const char* test_dirname_UUBAR = "./temp_v008/UUBAR";
+    const char* test_dirname_DDBAR = "./temp_v008/DDBAR";
+    const char* test_dirname_SSBAR = "./temp_v008/SSBAR";
+    const char* test_dirname_CHARM = "./temp_v008/CHARM";
     LetsFill(test_dirname_CHG, OBB_Oqq_BKG_test, OBB_BKG_test, Oqq_BKG_test, Scale_CHG_test);
     LetsFill(test_dirname_MIX, OBB_Oqq_BKG_test, OBB_BKG_test, Oqq_BKG_test, Scale_MIX_test);
     LetsFill(test_dirname_UUBAR, OBB_Oqq_BKG_test, OBB_BKG_test, Oqq_BKG_test, Scale_UUBAR_test);
@@ -285,6 +203,7 @@ void FastBDT_output_KS_test()
     double OBB_BKG_train_max = OBB_BKG_train->GetMaximum(); double OBB_SIGNAL_train_max = OBB_SIGNAL_train->GetMaximum();
     if(OBB_BKG_train_max > OBB_SIGNAL_train_max) OBB_BKG_train->SetMaximum(1.05 * OBB_BKG_train_max);
     else OBB_BKG_train->SetMaximum(1.05 * OBB_SIGNAL_train_max);
+    OBB_BKG_train->SetMinimum(0.03);
     OBB_BKG_train->Draw("Hist"); OBB_SIGNAL_train->Draw("HistSAME");
     OBB_BKG_test->Draw("AP SAME"); OBB_SIGNAL_test->Draw("AP SAME");
     TLegend* legend = gPad->BuildLegend(0.9, 0.9, 0.6, 0.6); legend->SetFillStyle(0); legend->SetLineWidth(0);
