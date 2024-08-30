ce467c12a5691fa1e6fd87179207e1c030d71a40
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-08-30 16:45:44 +0900

    fix fitter plot

--- NEW/include/Fitter.h
+++ NEW/include/Fitter.h
@@ -1144,7 +1144,7 @@ void GetPlotTemplate(RooWorkspace* w, RooDataSet* data = nullptr, const char * p
     TCanvas* c_temp = new TCanvas("c", "", 800, 800); c_temp->cd();
 
     TPad* pad1 = new TPad("pad1", "pad1", 0.0, 0.3, 1.0, 1.0);
-    pad1->SetBottomMargin(0.08); pad1->SetLeftMargin(0.15);
+    pad1->SetBottomMargin(0.02); pad1->SetLeftMargin(0.15);
     pad1->Draw(); pad1->cd();
 
     gStyle->SetPalette(kPastel);
@@ -1157,6 +1157,7 @@ void GetPlotTemplate(RooWorkspace* w, RooDataSet* data = nullptr, const char * p
 
     Stack->SetMaximum(real_max * 1.1);
 
+    Stack->GetXaxis()->SetLabelSize(0.0); Stack->GetXaxis()->SetTitleSize(0.0);
     Stack->Draw("pfc Hist");
     if (data != nullptr) {
         data_hist->SetLineWidth(2);
@@ -1184,7 +1185,7 @@ void GetPlotTemplate(RooWorkspace* w, RooDataSet* data = nullptr, const char * p
     pt_3->SetTextSize(0.04); pt_3->SetFillStyle(0); pt_3->SetLineWidth(0); pt_3->AddText("1.0 GeV/c^{2} < M_{X_{s}}^{reco}"); pt_3->Draw();
 
     c_temp->cd();
-    TPad* pad2 = new TPad("pad2", "pad2", 0.0, 0.0, 1, 0.3); pad2->SetBottomMargin(0.2); pad2->SetLeftMargin(0.15); pad2->Draw(); pad2->cd();
+    TPad* pad2 = new TPad("pad2", "pad2", 0.0, 0.0, 1, 0.3); pad2->SetBottomMargin(0.2); pad2->SetLeftMargin(0.15); pad2->SetTopMargin(0.2); pad2->Draw(); pad2->cd();
     Ratio_hist->SetMinimum(0.5); Ratio_hist->SetMaximum(1.5); Ratio_hist->SetLineWidth(2);
     Ratio_hist->GetYaxis()->SetTitleSize(0.08); Ratio_hist->GetYaxis()->SetTitleOffset(0.5); Ratio_hist->GetYaxis()->SetLabelSize(0.08);
     Ratio_hist->GetXaxis()->SetLabelSize(0.08); Ratio_hist->GetXaxis()->SetTitleSize(0.08);
