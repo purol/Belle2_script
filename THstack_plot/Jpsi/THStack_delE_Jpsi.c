80916f2a9bbc0721051b0ad5946aa64811e9826e
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-10-07 16:59:11 +0900

    use transparent legend for JPsi THPlot

--- THstack_plot/Jpsi/THStack_delE_Jpsi.c
+++ THstack_plot/Jpsi/THStack_delE_Jpsi.c
@@ -164,7 +164,7 @@ void THStack_delE_Jpsi() {
     Stack->Draw("pfc Hist"); SIGNAL_hist->Draw("HistSAME");
 
     TLegend* legend = gPad->BuildLegend(0.9, 0.9, 0.7, 0.7);
-    legend->SetFillStyle(0);
+    legend->SetFillStyle(0); legend->SetLineWidth(0);
 
     c_temp->SaveAs("Plot_delE_Jpsi.png");
 
