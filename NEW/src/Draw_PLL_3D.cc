72ac15cea5bc5f8f78682e31b9033824fa147181
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-11-07 23:30:53 +0900

    bug fix

--- NEW/src/Draw_PLL_3D.cc
+++ NEW/src/Draw_PLL_3D.cc
@@ -139,7 +139,7 @@ int main(int argc, char* argv[]) {
         for (double mu2_local = mu_MXs2_global - 3.5 * mu_MXs2_err; mu2_local < mu_MXs2_global + 3.5 * mu_MXs2_err; mu2_local = mu2_local + mu_MXs2_err * step) {
 
             double mu3_local = (target_BR - mu1_local * BR_1 - mu2_local * BR_2) / BR_3;
-            if ((mu3_local >= mu_MXs3_global - 3 * mu_MXs3_err) && (mu3_local < mu_MXs3_global + 3 * mu_MXs3_err)) {
+            if ((mu3_local >= mu_MXs3_global - 3.5 * mu_MXs3_err) && (mu3_local < mu_MXs3_global + 3.5 * mu_MXs3_err)) {
 
                 w->loadSnapshot("GlobalMinimumParamValues");
 
