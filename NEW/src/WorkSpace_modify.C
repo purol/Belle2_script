e0785f971ba28b994a2383198b38dd45cd90c854
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-11-15 23:59:29 +0900

    use simplified formular

--- NEW/src/WorkSpace_modify.C
+++ NEW/src/WorkSpace_modify.C
@@ -56,45 +56,64 @@ using namespace HistFactory;
 using std::string;
 using std::to_string;
 
-Corrector_Fragmentation corrector_Fragmentation;
-
 int WorkSpace_modify() {
 	// this code make several model with different \mu values
 
+	// read workspace
 	const char* fname = "./PDFandDATA_workspace.root";
 	TFile* f = TFile::Open(fname);
 	RooWorkspace* w = (RooWorkspace*)f->Get("combined");
 
+	// read model
 	RooStats::ModelConfig* sbModel = (RooStats::ModelConfig*)w->obj("ModelConfig");
 
-	for (int i = 0; i < 50; i++) {
-		RooStats::ModelConfig* temp_Model = (RooStats::ModelConfig*)sbModel->Clone(("ModelConfig_" + std::to_string(i)).c_str());
-		printf("print!\n");
+	// get BR for each MXs^true region
+	const double BR_1 = 0.0000048514;
+	const double BR_2 = 0.0000085024;
+	const double BR_3 = 0.0000156653;
+	const double BR_total = 0.000029;
 
-		const double BR_1 = 0.0000048514;
-		const double BR_2 = 0.0000085024;
-		const double BR_3 = 0.0000156653;
-		const double BR_total = 0.000029;
+	// read RooRealVar
+	RooRealVar* mu_MXs1 = w->var("mu_MXs1");
+	RooRealVar* mu_MXs2 = w->var("mu_MXs2");
+	RooRealVar* mu_MXs3 = w->var("mu_MXs3");
 
-		RooRealVar* mu_MXs1 = w->var("mu_MXs1");
-		RooRealVar* mu_MXs2 = w->var("mu_MXs2");
-		RooRealVar* mu_MXs3 = w->var("mu_MXs3");
+	// set mu_MXs1 to be constant
+	mu_MXs1->setConstant(true);
 
-		mu_MXs1->setConstant(true);
-		RooRealVar* mu = new RooRealVar("mu", "mu", -100.0, 100.0);
-		RooRealVar* mu1 = new RooFormulaVar("mu_MXs1_alt", "(@2*0.000029-@0*0.0000085024-@1*0.0000156653)/0.0000048514", RooArgList(*mu_MXs2, *mu_MXs3, *mu));
+	// define \mu and define another mu_MXs1
+	RooRealVar* mu = new RooRealVar("mu", "mu", 1.0, -100.0, 100.0);
+	RooFormulaVar* mu1 = new RooFormulaVar("mu_MXs1_alt", "@2*5.98159294224-@0*1.75256626953-@1*3.22902667271", RooArgList(*mu_MXs2, *mu_MXs3, *mu));
 
-		RooProduct* MXs1_MXs1_scale = (RooProduct*)w->function("Signal_MXs1_nominal_MXs1_channel_MXs1_scaleFactors");
-		MXs1_MXs1_scale->addTerm(mu1);
+	// use another mu_MXs1 as the scale factor
+	RooProduct* MXs1_MXs1_scale = (RooProduct*)w->function("Signal_MXs1_nominal_MXs1_channel_MXs1_scaleFactors");
+	MXs1_MXs1_scale->addTerm(mu1);
 
-		RooProduct* MXs1_MXs2_scale = (RooProduct*)w->function("Signal_MXs1_nominal_MXs2_channel_MXs2_scaleFactors");
-		MXs1_MXs2_scale->addTerm(mu1);
+	RooProduct* MXs1_MXs2_scale = (RooProduct*)w->function("Signal_MXs1_nominal_MXs2_channel_MXs2_scaleFactors");
+	MXs1_MXs2_scale->addTerm(mu1);
 
-		RooProduct* MXs1_MXs3_scale = (RooProduct*)w->function("Signal_MXs1_nominal_MXs3_channel_MXs3_scaleFactors");
-		MXs1_MXs3_scale->addTerm(mu1);
+	RooProduct* MXs1_MXs3_scale = (RooProduct*)w->function("Signal_MXs1_nominal_MXs3_channel_MXs3_scaleFactors");
+	MXs1_MXs3_scale->addTerm(mu1);
 
-		w->Print();
-	}
+	// set POI and parameter to be \mu, instead of mu_MXs1
+	w->import(*mu);
+	w->import(*mu1);
+	sbModel->SetParametersOfInterest(RooArgSet(*mu));
+	sbModel->SetParameters(RooArgSet(*mu));
+	//sbModel->GetPdf()->getVariables()->add(RooArgSet(*mu));
+	//const RooArgSet* POI = sbModel->GetParametersOfInterest();
+	//POI->Print();
+
+	//RooDataSet* data = (RooDataSet*)w->data("asimovData");
+	//sbModel->GetPdf()->getParameters(*data)->add(RooArgSet(*mu));
+	//sbModel->GetPdf()->getComponents();
+
+	// set snapshot
+	w->saveSnapshot("NominalParamValues", w->allVars());
+
+
+	//w->Print();
+	//w->writeToFile("PDFandDATA_workspace_mu.root");
 
 
 	return 0;
