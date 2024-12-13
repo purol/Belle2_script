1a57d731bf63423960908cc5efa7d3ff7c44ef3e
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2024-12-13 13:16:34 +0900

    bug fix

--- for_Zihan/MXs_dist.C
+++ for_Zihan/MXs_dist.C
@@ -8,7 +8,17 @@
 #include "TAxis.h"
 #include "RooPlot.h"
 
-void load_files(const char* dirname, std::vector<string>* names) {
+bool hasEnding(std::string const& fullString, std::string const& ending) {
+    if (fullString.length() >= ending.length()) {
+        return (0 == fullString.compare(fullString.length() - ending.length(), ending.length(), ending));
+    }
+    else {
+        return false;
+    }
+}
+
+
+void load_files(const char* dirname, std::vector<std::string>* names) {
     TSystemDirectory dir(dirname, dirname);
     TList* files = dir.GetListOfFiles();
     if (files) {
@@ -24,14 +34,30 @@ void load_files(const char* dirname, std::vector<string>* names) {
     }
 }
 
+void load_files(const char* dirname, std::vector<std::string>* names, const char* included_string) {
+    TSystemDirectory dir(dirname, dirname);
+    TList* files = dir.GetListOfFiles();
+    if (files) {
+        TSystemFile* file;
+        TString fname;
+        TIter next(files);
+        while ((file = (TSystemFile*)next())) {
+            fname = file->GetName();
+            if (!file->IsDirectory() && fname.EndsWith(".root") && fname.Contains(included_string)) {
+                names->push_back(fname.Data());
+            }
+        }
+    }
+}
+
 TH1D* MXs_11 = new TH1D("MXs_11", ";M_{Xs} [GeV];arbitrary unit", 30, 1.1, 4.0);
 TH1D* MXs_10 = new TH1D("MXs_10", ";M_{Xs} [GeV];arbitrary unit", 30, 1.1, 4.0);
 
-void LetsFill(const char* dirname, TH1D* MXs_hist, double weight_var = 1.0) {
+void LetsFill(const char* dirname, const char* included_string, TH1D* MXs_hist, double weight_var = 1.0) {
     double Mx = 0;
 
     std::vector<string> names;
-    load_files(dirname, &names);
+    load_files(dirname, &names, included_string);
 
     for (unsigned int i = 0; i < names.size(); i++) {
 
@@ -40,11 +66,14 @@ void LetsFill(const char* dirname, TH1D* MXs_hist, double weight_var = 1.0) {
 
         TTree* tree_upsilon = (TTree*)input_file->Get("Upsilon");
 
-        tree_upsilon->SetBranchAddress("test", &Mx);
+        tree_upsilon->SetBranchAddress("mcDaughter__bo0__cm__spmcDaughter__bo0__cm__spM__bc__bc", &Mx);
 
         printf("%lld entries...\n", tree_upsilon->GetEntries());
         for (unsigned int j = 0; j < tree_upsilon->GetEntries(); j++) { // Fill
             tree_upsilon->GetEntry(j);
+
+            // cut
+            if (Mx < 1.1) continue;
 
             MXs_hist->Fill(Mx, weight_var);
         }
@@ -58,30 +87,30 @@ void MXs_dist()
 {
 
     // get data from root files
-    const char* dirname = "";
+    const char* dirname = "./";
 
-    LetsFill(dirname, Oqq_SIGNAL_train, 1.0);
-    LetsFill(dirname, OBB_BKG_train, 1.0);
+    LetsFill(dirname, "B02Xsll_11", MXs_11, 1.0);
+    LetsFill(dirname, "B02Xsll_10", MXs_10, 1.0);
 
     double factor = 1.0;
 
     // normalization
-    Oqq_SIGNAL_train->Scale(factor / Oqq_SIGNAL_train->Integral(), "width");
-    OBB_BKG_train->Scale(factor / OBB_BKG_train->Integral(), "width");
+    MXs_11->Scale(factor / MXs_11->Integral(), "width");
+    MXs_10->Scale(factor / MXs_10->Integral(), "width");
 
-    Oqq_SIGNAL_train->SetFillStyle(3004);
-    Oqq_SIGNAL_train->SetLineColor(kBlue);
-    Oqq_SIGNAL_train->SetFillColor(kBlue);
+    MXs_11->SetFillStyle(3004);
+    MXs_11->SetLineColor(kBlue);
+    MXs_11->SetFillColor(kBlue);
 
-    OBB_BKG_train->SetFillStyle(3005);
-    OBB_BKG_train->SetLineColor(kRed);
-    OBB_BKG_train->SetFillColor(kRed);
+    MXs_10->SetFillStyle(3005);
+    MXs_10->SetLineColor(kRed);
+    MXs_10->SetFillColor(kRed);
 
     // draw plot and get p-value
     gStyle->SetOptStat(0);
 
     TCanvas* c_temp = new TCanvas("c", "", 600, 600); c_temp->cd(); gPad->SetLogy();
-    Oqq_SIGNAL_train->Draw("Hist"); OBB_BKG_train->Draw("HistSAME");
+    MXs_11->Draw("Hist"); MXs_10->Draw("HistSAME");
     TLegend* legend = gPad->BuildLegend(0.9, 0.9, 0.6, 0.6); legend->SetFillStyle(0); legend->SetLineWidth(0);
     c_temp->SaveAs("MXsll.png");
 
