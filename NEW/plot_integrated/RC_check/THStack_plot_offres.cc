47b64fd3849a6d7c2603f0d6eda6713c7d53322a
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-02-01 16:08:16 +0900

    print data MC ratio

--- NEW/plot_integrated/RC_check/THStack_plot_offres.cc
+++ NEW/plot_integrated/RC_check/THStack_plot_offres.cc
@@ -27,6 +27,10 @@ revise void Loader::ConvertIntoSeparateDataFile(std::string output_name, double 
 #include "TLegend.h"
 
 void main(int argc, char* argv[]) {
+    /*
+    * argv[1]: input version (ex. v031)
+    * argv[2]: output path
+    */
 
     NormalizeAtEachMXs = true;
 
@@ -680,8 +684,10 @@ void main(int argc, char* argv[]) {
 
     int index_bin_index = std::distance(variable_names.begin(), std::find(variable_names.begin(), variable_names.end(), "bin index"));
     FILE* fp;
-    fp = fopen((std::string(argv[2]) + "/CAL.txt").c_str(), "w");
-    fprintf("%lf %lf %lf\n", Ratio_Nevt_MXs1->GetBinContent(1), Ratio_Nevt_MXs2->GetBinContent(1), Ratio_Nevt_MXs3->GetBinContent(1));
+    fp = fopen((std::string(argv[2]) + "/MC_data_ratio.txt").c_str(), "w");
+    fprintf("%lf %lf\n", Ratio_Nevt_MXs1->GetBinContent(1), Ratio_Nevt_MXs1->GetBinError(1));
+    fprintf("%lf %lf\n", Ratio_Nevt_MXs2->GetBinContent(1), Ratio_Nevt_MXs2->GetBinError(1));
+    fprintf("%lf %lf\n", Ratio_Nevt_MXs3->GetBinContent(1), Ratio_Nevt_MXs3->GetBinError(1));
     fclose(fp);
 
     // free
