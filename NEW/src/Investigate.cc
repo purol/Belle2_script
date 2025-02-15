e97450967637d68b33eb88d6f72f017e4cd2e25f
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-02-15 23:43:37 +0900

    add CalculateEff_isSignal

---
+++ NEW/src/Investigate.cc
@@ -0,0 +1,62 @@
+// last update: 2023-07-14
+// for Belle2 data
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+#include "TFile.h"
+
+#include "constants.h"
+#include "base.h"
+#include "Loader.h"
+
+/*
+when you add new variables:
+revise # define N_Needed_info ...
+revise typedef struct data
+revise void Loader::GetData(TFile* input_file)
+check MakeShiftDoubleToInt if it affects the code
+revise void Loader::PrintRootFile(std::string output_name)
+revise void Loader::PrintSeparateRootFile(std::string output_name)
+revise void Loader::ConvertIntoSeparateDataFile(std::string output_name, double flag = 0)
+*/
+
+/* ====================================== */
+
+int main(int argc, char* argv[]) {
+    /*
+    * argv[1]: dirname
+    * argv[2]: outputname
+    * argv[3]: output path
+    * argv[4]: sample type: {SIGNAL|CHG|MIX|UUBAR|DDBAR|SSBAR|CHARM}
+    * argv[5]: MC version: {data|MC15ri|MC15rd}
+    * argv[6]: sample category: {test|train|validation}
+    */
+
+    std::vector<std::string> names;
+
+    load_files(argv[1], &names);
+
+    Loader loader;
+
+    for (unsigned int i = 0; i < names.size(); i++) {
+        loader.initialize();
+
+        TFile* input_file = new TFile((argv[1] + std::string("/") + names.at(i)).c_str(), "read");
+        printf("%s (%d/%zu)\n", ("Read " + names.at(i) + "... ").c_str(), i, names.size());
+        loader.GetData(input_file);
+        if (loader.event_info_is_valid() == false) { printf("error!\n"); return 0; }
+
+        std::string::size_type const p(names.at(i).find_last_of('.'));
+        std::string file_without_extension = names.at(i).substr(0, p);
+
+        loader.Cut(Loader::Btag, 0, Loader::larger_than, 1.5); 
+        loader.Cut(Loader::Btag, 0, Loader::smaller_than, 2.5);
+
+        loader.PrintRootFile("./output.root");
+    }
+    loader.End();
+
+    return 0;
+}
