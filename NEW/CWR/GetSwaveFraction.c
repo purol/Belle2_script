168e68bdff3fda93f2cd7643c3e1c33ffd75add8
unknown <junewoo@g.ecc.u-tokyo.ac.jp>
2025-05-03 02:19:38 +0900

    bug fix

--- NEW/CWR/GetSwaveFraction.c
+++ NEW/CWR/GetSwaveFraction.c
@@ -85,54 +85,52 @@ void GetSwaveFraction(){
 
     LetsFillMK0star(K0star_dirname, &list, "B02Kstar0nunu");
 
-    double N_total_796_996 = 0;
-    double N_total_644_1200 = 0;
+    double NP_total_796_996 = 0;
+    double NP_total_644_1200 = 0;
 
-    double avg_FS_796_996 = 0;
-    double avg_FS_644_1200 = 0;
+    double NS_total_796_996 = 0;
+    double NS_total_644_1200 = 0;
+
 
     for (int i = 0; i < list.size(); i++) {
         if ((0.796 < list.at(i).MKstar) && (list.at(i).MKstar < 0.996)) {
-            N_total_796_996 = N_total_796_996 + 1.0;
+            NP_total_796_996 = NP_total_796_996 + 1.0;
 
-            if ((list.at(i).q_square < 0.98)) avg_FS_796_996 = avg_FS_796_996 + 1.0 * 0.021; // assume
-            else if ((0.98 < list.at(i).q_square) && (list.at(i).q_square < 1.1)) avg_FS_796_996 = avg_FS_796_996 + 1.0 * (0.021 + 0.144) / 2.0; // interpolate
-            else if ((1.1 < list.at(i).q_square) && (list.at(i).q_square < 2.5)) avg_FS_796_996 = avg_FS_796_996 + 1.0 * 0.144;
-            else if ((2.5 < list.at(i).q_square) && (list.at(i).q_square < 4.0)) avg_FS_796_996 = avg_FS_796_996 + 1.0 * 0.029;
-            else if ((4.0 < list.at(i).q_square) && (list.at(i).q_square < 6.0)) avg_FS_796_996 = avg_FS_796_996 + 1.0 * 0.117;
-            else if ((6.0 < list.at(i).q_square) && (list.at(i).q_square < 8.0)) avg_FS_796_996 = avg_FS_796_996 + 1.0 * 0.033;
-            else if ((8.0 < list.at(i).q_square) && (list.at(i).q_square < 11.0)) avg_FS_796_996 = avg_FS_796_996 + 1.0 * (0.033 + 0.021) / 2.0; // interpolate
-            else if ((11.0 < list.at(i).q_square) && (list.at(i).q_square < 12.5)) avg_FS_796_996 = avg_FS_796_996 + 1.0 * 0.021;
-            else if ((12.5 < list.at(i).q_square) && (list.at(i).q_square < 15.0)) avg_FS_796_996 = avg_FS_796_996 + 1.0 * (0.021 - 0.008) / 2.0; // interpolate
-            else if ((15.0 < list.at(i).q_square) && (list.at(i).q_square < 17.0)) avg_FS_796_996 = avg_FS_796_996 + 1.0 * (-0.008);
-            else if (17.0 < list.at(i).q_square) avg_FS_796_996 = avg_FS_796_996 + 1.0 * 0.144; // assume
+            if ((list.at(i).q_square < 0.98)) NS_total_796_996 = NS_total_796_996 + 1.0 * 0.021 / (1 - 0.021); // assume
+            else if ((0.98 < list.at(i).q_square) && (list.at(i).q_square < 1.1)) NS_total_796_996 = NS_total_796_996 + 1.0 * 0.0825 / (1 - 0.0825); // interpolate
+            else if ((1.1 < list.at(i).q_square) && (list.at(i).q_square < 2.5)) NS_total_796_996 = NS_total_796_996 + 1.0 * 0.144 / (1 - 0.144);
+            else if ((2.5 < list.at(i).q_square) && (list.at(i).q_square < 4.0)) NS_total_796_996 = NS_total_796_996 + 1.0 * 0.029 / (1 - 0.029);
+            else if ((4.0 < list.at(i).q_square) && (list.at(i).q_square < 6.0)) NS_total_796_996 = NS_total_796_996 + 1.0 * 0.117 / (1 - 0.117);
+            else if ((6.0 < list.at(i).q_square) && (list.at(i).q_square < 8.0)) NS_total_796_996 = NS_total_796_996 + 1.0 * 0.033 / (1 - 0.033);
+            else if ((8.0 < list.at(i).q_square) && (list.at(i).q_square < 11.0)) NS_total_796_996 = NS_total_796_996 + 1.0 * 0.027 / (1 - 0.027); // interpolate
+            else if ((11.0 < list.at(i).q_square) && (list.at(i).q_square < 12.5)) NS_total_796_996 = NS_total_796_996 + 1.0 * 0.021 / (1 - 0.021);
+            else if ((12.5 < list.at(i).q_square) && (list.at(i).q_square < 15.0)) NS_total_796_996 = NS_total_796_996 + 1.0 * 0.0065 / (1 - 0.0065); // interpolate
+            else if ((15.0 < list.at(i).q_square) && (list.at(i).q_square < 17.0)) NS_total_796_996 = NS_total_796_996 + 1.0 * (-0.008) / (1 + 0.008);
+            else if (17.0 < list.at(i).q_square) NS_total_796_996 = NS_total_796_996 + 1.0 * 0.144 / (1 - 0.144); // assume
 
         }
 
 
 
         if ((0.644 < list.at(i).MKstar) && (list.at(i).MKstar < 1.200)) {
-            N_total_644_1200 = N_total_644_1200 + 1.0;
+            NP_total_644_1200 = NP_total_644_1200 + 1.0;
 
-            if ((list.at(i).q_square < 0.98)) avg_FS_644_1200 = avg_FS_644_1200 + 1.0 * 0.021; // assume
-            else if ((0.98 < list.at(i).q_square) && (list.at(i).q_square < 1.1)) avg_FS_644_1200 = avg_FS_644_1200 + 1.0 * (0.021 + 0.144) / 2.0; // interpolate
-            else if ((1.1 < list.at(i).q_square) && (list.at(i).q_square < 2.5)) avg_FS_644_1200 = avg_FS_644_1200 + 1.0 * 0.144;
-            else if ((2.5 < list.at(i).q_square) && (list.at(i).q_square < 4.0)) avg_FS_644_1200 = avg_FS_644_1200 + 1.0 * 0.029;
-            else if ((4.0 < list.at(i).q_square) && (list.at(i).q_square < 6.0)) avg_FS_644_1200 = avg_FS_644_1200 + 1.0 * 0.117;
-            else if ((6.0 < list.at(i).q_square) && (list.at(i).q_square < 8.0)) avg_FS_644_1200 = avg_FS_644_1200 + 1.0 * 0.033;
-            else if ((8.0 < list.at(i).q_square) && (list.at(i).q_square < 11.0)) avg_FS_644_1200 = avg_FS_644_1200 + 1.0 * (0.033 + 0.021) / 2.0; // interpolate
-            else if ((11.0 < list.at(i).q_square) && (list.at(i).q_square < 12.5)) avg_FS_644_1200 = avg_FS_644_1200 + 1.0 * 0.021;
-            else if ((12.5 < list.at(i).q_square) && (list.at(i).q_square < 15.0)) avg_FS_644_1200 = avg_FS_644_1200 + 1.0 * (0.021 - 0.008) / 2.0; // interpolate
-            else if ((15.0 < list.at(i).q_square) && (list.at(i).q_square < 17.0)) avg_FS_644_1200 = avg_FS_644_1200 + 1.0 * (-0.008);
-            else if (17.0 < list.at(i).q_square) avg_FS_644_1200 = avg_FS_644_1200 + 1.0 * 0.144; // assume
+            if ((list.at(i).q_square < 0.98)) NS_total_644_1200 = NS_total_644_1200 + 1.0 * 0.021 / (1 - 0.021); // assume
+            else if ((0.98 < list.at(i).q_square) && (list.at(i).q_square < 1.1)) NS_total_644_1200 = NS_total_644_1200 + 1.0 * 0.0825 / (1 - 0.0825); // interpolate
+            else if ((1.1 < list.at(i).q_square) && (list.at(i).q_square < 2.5)) NS_total_644_1200 = NS_total_644_1200 + 1.0 * 0.144 / (1 - 0.144);
+            else if ((2.5 < list.at(i).q_square) && (list.at(i).q_square < 4.0)) NS_total_644_1200 = NS_total_644_1200 + 1.0 * 0.029 / (1 - 0.029);
+            else if ((4.0 < list.at(i).q_square) && (list.at(i).q_square < 6.0)) NS_total_644_1200 = NS_total_644_1200 + 1.0 * 0.117 / (1 - 0.117);
+            else if ((6.0 < list.at(i).q_square) && (list.at(i).q_square < 8.0)) NS_total_644_1200 = NS_total_644_1200 + 1.0 * 0.033 / (1 - 0.033);
+            else if ((8.0 < list.at(i).q_square) && (list.at(i).q_square < 11.0)) NS_total_644_1200 = NS_total_644_1200 + 1.0 * 0.027 / (1 - 0.027); // interpolate
+            else if ((11.0 < list.at(i).q_square) && (list.at(i).q_square < 12.5)) NS_total_644_1200 = NS_total_644_1200 + 1.0 * 0.021 / (1 - 0.021);
+            else if ((12.5 < list.at(i).q_square) && (list.at(i).q_square < 15.0)) NS_total_644_1200 = NS_total_644_1200 + 1.0 * 0.0065 / (1 - 0.0065); // interpolate
+            else if ((15.0 < list.at(i).q_square) && (list.at(i).q_square < 17.0)) NS_total_644_1200 = NS_total_644_1200 + 1.0 * (-0.008) / (1 + 0.008);
+            else if (17.0 < list.at(i).q_square) NS_total_644_1200 = NS_total_644_1200 + 1.0 * 0.144 / (1 - 0.144); // assume
         }
 
     }
 
-    avg_FS_796_996 = avg_FS_796_996 / N_total_796_996;
-    avg_FS_644_1200 = avg_FS_644_1200 / N_total_644_1200;
-
-    printf("%lf %lf\n", avg_FS_796_996, avg_FS_644_1200);
+    printf("%lf %lf\n", (NS_total_796_996 / (NS_total_796_996 + NP_total_796_996)), (NS_total_644_1200 / (NS_total_644_1200 + NP_total_644_1200)));
 
 
 }
